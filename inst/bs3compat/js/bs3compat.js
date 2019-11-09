a22e73cbaf2ba25cc29bae6081ac68da8369b671
Joe Cheng <joe@rstudio.com>
2019-11-08 16:25:06 -0800

    Implement $(el).tab("show") for both bs3 and bs4

--- inst/bs3compat/js/bs3compat.js
+++ inst/bs3compat/js/bs3compat.js
@@ -1,10 +1,15 @@
 (function($) {
-  if ($.fn.tab.Constructor.VERSION !== "3.4.1") {
+  if (!$.fn.tab.Constructor.VERSION.match(/^3\./)) {
     (console.warn || console.error || console.log)("bs3compat.js couldn't find bs3 tab impl; bs3 tabs will not be properly supported");
     return;
   }
-
   var bs3TabPlugin = $.fn.tab.noConflict();
+
+  if (!$.fn.tab.Constructor.VERSION.match(/^4\./)) {
+    (console.warn || console.error || console.log)("bs3compat.js couldn't find bs4 tab impl; bs3 tabs will not be properly supported");
+    return;
+  }
+  var bs4TabPlugin = $.fn.tab.noConflict();
 
   var EVENT_KEY = "click.bs.tab.data-api";
   var SELECTOR = '[data-toggle="tab"], [data-toggle="pill"]';
@@ -12,16 +17,26 @@
   $(document).off(EVENT_KEY);
   $(document).on(EVENT_KEY, SELECTOR, function(event) {
     event.preventDefault();
+    $(this).tab("show");
+  });
 
-    var previous = $(this).closest(".nav").find(".active");
-    if (previous[0] && previous[0].nodeName === "LI") {
+  function TabPlugin(config) {
+    if ($(this).closest(".nav").find(".nav-item, .nav-link").length === 0) {
       // Bootstrap 3 tabs detected
-      bs3TabPlugin.call($(this), "show");
+      bs3TabPlugin.call($(this), config);
     } else {
       // Bootstrap 4 tabs detected
-      $(this).tab("show");
+      bs4TabPlugin.call($(this), config);
     }
-  });
+  }
+
+  var noconflict = $.fn.tab;
+  $.fn.tab = TabPlugin;
+  $.fn.tab.Constructor = bs4TabPlugin.Constructor;
+  $.fn.tab.noConflict = function() {
+    $.fn.tab = noconflict;
+    return TabPlugin;
+  ;}
 
 })(jQuery);
 
