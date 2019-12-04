d38884b82c7007bb9283875c57123a79623ee8cc
Carson <cpsievert1@gmail.com>
2019-12-04 09:46:51 -0600

    have navbar_height() support version 3 as well

--- R/navbar.R
+++ R/navbar.R
@@ -7,34 +7,58 @@
 #'
 #' @param theme a <https://bootswatch.com/> theme name. The default value
 #' means vanilla Bootstrap (i.e. no bootswatch theme).
+#' @param version
 #' @export
-navbar_height <- function(theme = "") {
+navbar_height <- function(theme = "", version = 4) {
+
+  if (version == 3) {
+    return(switch(
+      theme,
+      journal = 61,
+      flatly = 60,
+      darkly = 60,
+      readable = 66,
+      spacelab = 52,
+      united = 51,
+      cosmo = 51,
+      lumen = 54,
+      paper = 64,
+      sandstone = 61,
+      simplex = 41,
+      yeti = 45,
+      51))
+  }
+
 
   # TODO: it'd be great if, someday, this took into account SASS variables,
   # but it's not immediately obvious how to do that correctly
-  switch(
-    theme,
-    cerulean = 56,
-    cosmo = 54.5,
-    cyborg = 53,
-    darkly = 70.5,
-    flatly = 70.5,
-    journal = 61,
-    litera = 59.5,
-    lumen = 57,
-    lux = 91.25,
-    materia = 80.3,
-    minty = 56,
-    pulse = 75.4,
-    sandstone = 54,
-    simplex = 66.3,
-    sketchy = 58,
-    slate = 56.5,
-    solar = 56,
-    spacelab = 58,
-    superhero = 48,
-    united = 56,
-    yeti = 54.5,
-    53
-  )
+  if (version == 4) {
+    return(switch(
+      theme,
+      cerulean = 56,
+      cosmo = 54.5,
+      cyborg = 53,
+      darkly = 70.5,
+      flatly = 70.5,
+      journal = 61,
+      litera = 59.5,
+      lumen = 57,
+      lux = 91.25,
+      materia = 80.3,
+      minty = 56,
+      pulse = 75.4,
+      sandstone = 54,
+      simplex = 66.3,
+      sketchy = 58,
+      slate = 56.5,
+      solar = 56,
+      spacelab = 58,
+      superhero = 48,
+      united = 56,
+      yeti = 54.5,
+      53
+    ))
+  }
+
+  stop("Bootstrap version not supported", version, call. = FALSE)
 }
