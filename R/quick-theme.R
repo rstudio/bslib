b3e67d624badbc82871d3f7f93ff6a76007db642
Joe Cheng <joe@rstudio.com>
2020-02-17 15:00:42 -0800

    Implement bs_quick_theme for bs3

--- R/quick-theme.R
+++ R/quick-theme.R
@@ -4,15 +4,22 @@ bs_quick_theme <- function(bg = "#FFFFFF", fg = "#000000",
 
   theme <- bs_theme_get()
   if (is.null(theme)) {
-    stop(
-      "No bootstraplib theme is active (did you forget to call bs_theme_new()?)")
+    stop("No bootstraplib theme is active (did you forget to call bs_theme_new()?)")
   }
 
-  valid_themes <- c("bootstraplib_version_4", "bootstraplib_version_4+3")
-  if (!any(valid_themes %in% theme$tags)) {
-    stop("Sorry, bs_quick_theme requires Bootstrap 4 (see ?bs_theme_new to specify version)")
-  }
+  bs4_tags <- c("bootstraplib_version_4", "bootstraplib_version_4+3")
+  bs3_tags <- c("bootstraplib_version_3")
 
+  if (any(bs4_tags %in% theme$tags)) {
+    bs4_quick_theme(bg, fg, accent, secondary)
+  } else if (any(bs3_tags %in% theme$tags)) {
+    bs3_quick_theme(bg, fg, accent, secondary)
+  } else {
+    stop("bs_quick_theme doesn't recognize the active version of Bootstrap")
+  }
+}
+
+bs4_quick_theme <- function(bg, fg, accent, secondary) {
   white <- shiny:::parseCssColor(bg)
   black <- shiny:::parseCssColor(fg)
 
@@ -58,6 +65,99 @@ bs_quick_theme <- function(bg = "#FFFFFF", fg = "#000000",
       "yiq-text-light" = grays[["gray-900"]],
       "yiq-text-dark" = grays[["white"]]
     )
+  }
+
+  invisible()
+}
+
+bs3_quick_theme <- function(bg, fg, accent, secondary) {
+  # TODO: normalize bg and fg into #RRGGBB
+  white <- shiny:::parseCssColor(bg)
+  black <- shiny:::parseCssColor(fg)
+
+  if (!is.null(secondary)) {
+    warning(call. = FALSE,
+      "bs_quick_theme's `secondary` argument is not currently supported for ",
+      "Bootstrap 3"
+    )
+  }
+
+  gray <- function(level = 255) {
+    val <- round(colorRamp(c(black, white))(level / 255))
+    sprintf("#%02X%02X%02X", val[1,1], val[1,2], val[1,3])
+  }
+
+  result_colors <- list(
+    "gray-base" = gray(0x00),
+    "gray-darker" = gray(0x22),
+    "gray-dark" = gray(0x33),
+    "gray" = gray(0x55),
+    "gray-light" = gray(0x77),
+    "gray-lighter" = gray(0xEE),
+
+    "gray-44" = gray(0x44),
+    "gray-88" = gray(0x88),
+    "gray-99" = gray(0x99),
+    "gray-cc" = gray(0xcc),
+    "gray-dd" = gray(0xdd),
+    "gray-e5" = gray(0xe5),
+    "gray-f5" = gray(0xf5),
+    "gray-f8" = gray(0xf8),
+    "gray-f9" = gray(0xf9),
+    "white" = gray(0xff)
+  )
+
+  # There's code in tools/bs3_quick_theme.R for generating this list.
+  color_mapping <- list(`dropdown-caret-color` = "$gray-base", `tooltip-bg` = "$gray-base",
+    `modal-backdrop-bg` = "$gray-base", `close-color` = "$gray-base",
+    `navbar-inverse-bg` = "$gray-darker", `btn-default-color` = "$gray-dark",
+    `navbar-default-link-hover-color` = "$gray-dark", `navbar-inverse-toggle-hover-bg` = "$gray-dark",
+    `navbar-inverse-toggle-border-color` = "$gray-dark", `list-group-link-heading-color` = "$gray-dark",
+    `kbd-bg` = "$gray-dark", `navbar-inverse-link-disabled-color` = "$gray-44",
+    `navbar-default-link-active-color` = "$gray", `list-group-link-color` = "$gray",
+    `navbar-default-color` = "$gray-light", `navbar-default-link-color` = "$gray-light",
+    `navbar-default-toggle-icon-bar-bg` = "$gray-88", `input-color-placeholder` = "$gray-99",
+    `modal-content-fallback-border-color` = "$gray-99", `btn-default-border` = "$gray-cc",
+    `input-border` = "$gray-cc", `dropdown-fallback-border` = "$gray-cc",
+    `navbar-default-link-disabled-color` = "$gray-cc", `popover-fallback-border-color` = "$gray-cc",
+    `breadcrumb-color` = "$gray-cc", `pre-border-color` = "$gray-cc",
+    `table-border-color` = "$gray-dd", `navbar-default-toggle-hover-bg` = "$gray-dd",
+    `navbar-default-toggle-border-color` = "$gray-dd", `nav-tabs-border-color` = "$gray-dd",
+    `nav-tabs-active-link-hover-border-color` = "$gray-dd", `nav-tabs-justified-link-border-color` = "$gray-dd",
+    `pagination-border` = "$gray-dd", `pagination-hover-border` = "$gray-dd",
+    `pagination-disabled-border` = "$gray-dd", `list-group-border` = "$gray-dd",
+    `panel-inner-border` = "$gray-dd", `panel-default-border` = "$gray-dd",
+    `thumbnail-border` = "$gray-dd", `legend-border-color` = "$gray-e5",
+    `dropdown-divider-bg` = "$gray-e5", `modal-header-border-color` = "$gray-e5",
+    `table-bg-hover` = "$gray-f5", `dropdown-link-hover-bg` = "$gray-f5",
+    `progress-bg` = "$gray-f5", `list-group-hover-bg` = "$gray-f5",
+    `panel-footer-bg` = "$gray-f5", `panel-default-heading-bg` = "$gray-f5",
+    `well-bg` = "$gray-f5", `breadcrumb-bg` = "$gray-f5", `pre-bg` = "$gray-f5",
+    `navbar-default-bg` = "$gray-f8", `table-bg-accent` = "$gray-f9",
+    `body-bg` = "$white", `component-active-color` = "$white",
+    `btn-default-bg` = "$white", `btn-primary-color` = "$white",
+    `btn-success-color` = "$white", `btn-info-color` = "$white",
+    `btn-warning-color` = "$white", `btn-danger-color` = "$white",
+    `input-bg` = "$white", `dropdown-bg` = "$white", `navbar-inverse-link-hover-color` = "$white",
+    `navbar-inverse-brand-hover-color` = "$white", `navbar-inverse-toggle-icon-bar-bg` = "$white",
+    `pagination-bg` = "$white", `pagination-active-color` = "$white",
+    `pagination-disabled-bg` = "$white", `tooltip-color` = "$white",
+    `popover-bg` = "$white", `label-color` = "$white", `label-link-hover-color` = "$white",
+    `modal-content-bg` = "$white", `progress-bar-color` = "$white",
+    `list-group-bg` = "$white", `panel-bg` = "$white", `panel-primary-text` = "$white",
+    `badge-color` = "$white", `badge-link-hover-color` = "$white",
+    `badge-active-bg` = "$white", `carousel-control-color` = "$white",
+    `carousel-indicator-active-bg` = "$white", `carousel-indicator-border-color` = "$white",
+    `carousel-caption-color` = "$white", `close-text-shadow` = "$white",
+    `kbd-color` = "$white")
+
+  bs_theme_add_variables(
+    !!!result_colors,
+    !!!color_mapping
+  )
+
+  if (!is.null(accent)) {
+    bs_theme_add_variables("brand-primary" = accent)
   }
 
   invisible()
---
+++ tools/bs3_quick_theme.R
@@ -0,0 +1,70 @@
+# This script generates code for replacing hardcoded colors for Bootstrap 3.
+# Unlike Bootstrap 4, the Bootstrap 3 _variables.scss has a lot of repeated
+# color literals. We want to replace all of the ones that are monochrome with
+# dynamically calculated replacements.
+
+# Load bootstrap-sass (bs3) _variables.scss
+lines <- readLines("https://raw.githubusercontent.com/twbs/bootstrap-sass/master/assets/stylesheets/bootstrap/_variables.scss")
+# Retain lines that seem like they contain a hex color.
+# (Note that colors specified by keyword would break this logic, but, AFAICT,
+# there aren't any such colors in _variables.scss.)
+lines <- lines[grepl("#[0-9a-f-A-F]", lines, perl = TRUE)]
+
+# Parse each line into variable name and hex color
+df <- data.frame(stringsAsFactors = FALSE,
+  name = sub("^\\$(.+?):.*", "\\1", lines, perl = TRUE),
+  color = sub(".*(#[0-9A-Za-z]+).*", "\\1", lines, perl = TRUE)
+)
+# Sort by color value
+df <- df[order(df$color),]
+
+# All of the color values that we want to replace, are assigned a variable name.
+# Any variables in _variables.scss that point to one of these color values, will
+# now point to the variable name by default. For example, since _variables.scss
+# contains this:
+#
+#   $navbar-inverse-bg: #222 !default;
+#
+# Our goal is to generate the rule:
+#
+#   $navbar-inverse-bg: $gray-darker !default;
+#
+# So that all such variables (i.e. ones whose default value is #222) can be
+# changed just by modifying $gray-darker.
+named_colors <- c(
+  # These variables already exist in _variables.scss
+  "gray-base" = "#000",
+  "gray-darker" = "#222",
+  "gray-dark" = "#333",
+  "gray" = "#555",
+  "gray-light" = "#777",
+  "gray-lighter" = "#eee",
+
+  # These are variables we're introducing
+  "gray-44" = "#444",
+  "gray-88" = "#888",
+  "gray-99" = "#999",
+  "gray-cc" = "#ccc",
+  "gray-dd" = "#ddd",
+  "gray-e5" = "#e5e5e5",
+  "gray-f5" = "#f5f5f5",
+  "gray-f8" = "#f8f8f8",
+  "gray-f9" = "#f9f9f9",
+  "white" = "#fff"
+)
+
+# Color variables in _variables.scss that we don't have names for.
+# Consider this a potential TODO list of colors we could dynamically set.
+df[!df$color %in% named_colors,]
+
+# Generate the code for color_mapping in bs3_quick_theme
+df %>%
+  mutate(varname = names(named_colors[match(color, named_colors)])) %>%
+  filter(!is.na(varname)) %>%
+  filter(name != varname) %>%
+  mutate(varname = paste0("$", varname)) %>%
+  select(name, varname) %>%
+  { setNames(.$varname, .$name) } %>%
+  as.list() %>%
+  dput()
+
