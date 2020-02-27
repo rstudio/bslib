e2bef51e37e993c6d80e0ae6ae689c4b69a415a8
Carson <cpsievert1@gmail.com>
2020-02-27 16:38:56 -0600

    At build-time, add known vendor prefixes to bootstrap sass and throw if we distributed css contains prefixes we don't recognize

--- tools/yarn_install.R
+++ tools/yarn_install.R
@@ -1,8 +1,128 @@
+library(sass)
+
 if (Sys.which("yarn") == "") {
   stop("The yarn CLI must be installed and in your PATH")
 }
 
 withr::with_dir("inst", system("yarn install"))
+
+# ----------------------------------------------------------------------
+# Add known vendor prefixes to bootstrap.scss (since we don't want
+# to rely on a node run-time to prefix after compilation)
+# https://github.com/twbs/bootstrap/blob/d438f3/package.json#L30
+# ----------------------------------------------------------------------
+
+scss_files <- dir(
+  "inst/node_modules/bootstrap/scss",
+  recursive = TRUE, full.names = TRUE
+)
+scss_src <- lapply(scss_files, readLines)
+
+add_prefixes <- function(src, property, ok_values = NULL) {
+  pattern <- paste0("^\\s*", property, ":\\s*(.+);")
+  idx <- grep(pattern, src)
+  for (i in idx) {
+    prop <- src[[i]]
+    if (length(ok_values)) {
+      value <- regmatches(prop, regexec(pattern, prop))[[1]][2]
+      vals <- strsplit(value, "\\s+")[[1]]
+      if (all(grepl(paste(ok_values, collapse = "|"), vals))) next
+    }
+    leading_ws <- regmatches(prop, regexpr("^\\s+", prop))
+    prop_prefixes <- paste0(
+      leading_ws,
+      c("-webkit-", "-moz-", "-ms-"),
+      sub("^\\s+", "", prop),
+      collapse = "\n"
+    )
+    src[[i]] <- paste0(prop, "\n", prop_prefixes)
+  }
+  src
+}
+
+# Unconditionally prefix the following CSS properties
+needs_prefix <- c(
+  "appearance", "user-select", "backdrop-filter",
+  "backface-visibility", "touch-action"
+)
+for (prop in needs_prefix) {
+  scss_src <- lapply(scss_src, add_prefixes, prop)
+}
+# Conditionally prefix text-decoration if its not CSS2 compliant
+# https://www.w3.org/TR/CSS2/text.html#lining-striking-props
+# https://developer.mozilla.org/en-US/docs/Web/CSS/text-decoration
+# https://caniuse.com/#search=text-decoration
+scss_src <- lapply(scss_src, add_prefixes, "text-decoration", c("none", "underline", "overline", "line-through", "blink", "inherit"))
+
+# Write modified source to disk
+invisible(Map(writeLines, scss_src, scss_files))
+
+# ----------------------------------------------------------------------
+# Check to make sure we're not missing any vendor prefixes
+# that we don't already know about in the distributed CSS
+# ----------------------------------------------------------------------
+
+find_prefixed_css <- function(css) {
+  vendors <- c("webkit", "moz", "ms")
+  prefixes <- lapply(vendors, function(vendor) {
+    pattern <- sprintf("-%s-([a-z-]+)", vendor)
+    prefixes <- regmatches(css, regexec(pattern, css))
+    lapply(prefixes, function(x) if (length(x) > 1) x[2] else NULL)
+  })
+  unique(unlist(prefixes, recursive = TRUE))
+}
+
+src_prefixes <- find_prefixed_css(unlist(scss_src))
+dist_prefixes <- find_prefixed_css(
+  readLines("inst/node_modules/bootstrap/dist/css/bootstrap.css")
+)
+auto_prefixes <- setdiff(dist_prefixes, src_prefixes)
+
+whitelist <- c(
+  # https://caniuse.com/#feat=flexbox
+  "flex", "inline-flex", "inline-flexbox",
+  # https://caniuse.com/#feat=mdn-api_csskeyframesrule
+  "keyframes",
+  # https://caniuse.com/#feat=mdn-css_properties_transition
+  "transition",
+  # https://caniuse.com/#feat=css-animation
+  "animation",
+  # https://caniuse.com/#feat=transforms2d
+  "transform",
+  # https://caniuse.com/#feat=mdn-css_properties_column-count
+  "column-count",
+  # https://caniuse.com/#feat=mdn-css_properties_column-gap_multicol_context
+  "column-gap",
+  # https://caniuse.com/#feat=css-placeholder
+  "placeholder", "input-placeholder",
+  # https://developer.mozilla.org/en-US/docs/Web/CSS/text-decoration-skip-ink
+  "text-decoration-skip-ink",
+  # webkit prefix doesn't seem necessary? And Bootstrap docs warn about IE...
+  # https://caniuse.com/#search=sticky
+  "sticky",
+  # Already applied conditional prefixing
+  "text-decoration"
+)
+
+unknown_prefixes <- setdiff(
+  # whitelist flexbox props (there are many)
+  auto_prefixes[!grepl("^flex-*", auto_prefixes)],
+  whitelist
+)
+if (length(unknown_prefixes)) {
+  stop(
+    "Unknown vendor prefixes introduced by Bootstrap's autoprefixer. ",
+    "Use either add_prefixes() to add prefixes or whitelist them ",
+    "(if they're not needed for modern browsers): ",
+    "'", paste(collapse = "', '", unknown_prefixes), "'.",
+    call. = FALSE
+  )
+}
+
+
+# ----------------------------------------------------------------------
+# Now, get rid of files that we don't need to bundle with the package
+# ----------------------------------------------------------------------
 
 # I don't think we'll need these special builds of popper (at least for now)
 unlink("inst/node_modules/popper.js/dist/umd", recursive = TRUE)
@@ -50,8 +170,12 @@ unlink("inst/node_modules/bootswatch3/fonts", recursive = TRUE)
 
 # Rename node_modules to lib to avoid this CRAN note
 # https://www.r-bloggers.com/the-most-annoying-warning-for-cran-submission/
-pkgs <- Sys.glob("inst/node_modules/*")
-file.rename(pkgs, sub("node_modules/", "lib/", pkgs))
+pkg_files <- dir("inst/node_modules", recursive = TRUE, full.names = TRUE)
+file.copy(
+  pkg_files,
+  sub("node_modules/", "lib/", pkg_files),
+  recursive = TRUE
+)
 unlink("inst/node_modules/", recursive = TRUE)
 
 # GitHub reports security issues of devDependencies, but that's irrelevant to us
@@ -66,7 +190,7 @@ invisible(lapply(Sys.glob("inst/lib/*/package.json"), remove_dev_dependencies))
 # Get BS4/BS3 versions (for bootstraplib::bootstrap() versioning)
 version_bs4 <- jsonlite::fromJSON("inst/lib/bootstrap/package.json")$version
 version_bs3 <- jsonlite::fromJSON("inst/lib/bootstrap-sass/package.json")$version
-usethis::use_data(version_bs4, version_bs3, internal = TRUE)
+usethis::use_data(version_bs4, version_bs3, internal = TRUE, overwrite = TRUE)
 
 # Create the LICENSE file
 LICENSE <- c(
