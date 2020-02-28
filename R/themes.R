73738116c43d5f89d0b8cc61d72b84b874a6787a
Carson <cpsievert1@gmail.com>
2020-02-28 10:44:36 -0600

    Improvements to bs_theme_*() Rd docs

--- R/themes.R
+++ R/themes.R
@@ -3,168 +3,70 @@
 #' `bs_theme_new()` creates a new (global) Bootstrap Sass theme which
 #' [bootstrap()] (or [bootstrap_sass()]) can consume (their `theme` argument
 #' defaults to `bs_theme_get()`, which get the current global theme). Once a
-#' global theme has been created, use [bs_theme_add_variables()] to set
-#' Sass variable defaults and [bs_theme_add()] to add arbitrary Sass
-#' (via [sass::sass_layer()]s).
+#' global theme has been created, theme customization may be added via
+#' 'high-level' (e.g., [bs_theme_base_colors()])
+#' and/or 'low-level' (e.g., [bs_theme_add_variables()]) customizations.
 #'
 #' @param version The major version of Bootstrap to use. A value of
 #' `'4+3'` means Bootstrap 4, but with additional CSS/JS to support
 #' BS3 style markup in BS4. Other supported versions include 3 and 4.
 #' @param bootswatch The name of a bootswatch theme.
 #' See [bootswatch_themes()] to list possible names.
-#' @param defaults Any [sass::as_sass()] `input` to place before Bootstrap's Sass.
-#' @param declarations Any [sass::as_sass()] `input` to place after Bootstrap's variables,
-#' functions, and mixins, but before Bootstrap's styling rules.
-#' @param rules Any [sass::as_sass()] `input` to place after Bootstrap's Sass imports.
-#' @param ... For `bs_theme_add_variables()`, these arguments define Sass variables;
-#' otherwise, these arguments are passed along to [sass::sass_layer()].
-#' @param .where whether to place the variable definitions before other
-#' Sass `"defaults"`, after other Sass `"declarations"`, or after other Sass `"rules"`.
-#' @param .default_flag whether or not to add a `!default` flag (if missing) to
-#' variable expressions. It's recommended to keep this as `TRUE` when
-#' `.where = "defaults"`.
 #'
 #' @references \url{https://getbootstrap.com/docs/4.4/getting-started/theming/}
 #' @references \url{https://rstudio.github.io/sass/}
 #'
+#' @seealso [bootstrap()], [bs_theme_preview()], [bs_theme_base_colors()], [bs_theme_add_variables()]
 #' @rdname theming
 #' @export
 #' @examples
 #'
-#' # Function to preview the styling a (primary) Bootstrap button
-#' library(htmltools)
-#' button <- tags$a(class = "btn btn-primary", href = "#", role = "button", "Hello")
-#' preview_button <- function(x) {
-#'   browsable(tags$body(x, button))
-#' }
+#' # Themes are stored globally, and if there is no theme present,
+#' # bs_theme_get() returns NULL
+#' bs_theme_get()
 #'
-#' # To create a custom theme, you must start by calling bs_theme_new()
-#' # Here we start with a theme based on a Bootswatch theme,
-#' # then override some variable defaults
-#' bs_theme_new(bootswatch = "sketchy")
-#' bs_theme_add_variables(
-#'   primary = "orange",
-#'   "body-bg" = "#EEEEEE",
-#'   "font-family-base" = "monospace",
-#'   "font-size-base" = "1.4rem",
-#'   "btn-padding-y" = ".16rem",
-#'   "btn-padding-x" = "2rem",
-#'   "border-radius" = 0,
-#'   "border-radius-lg" = 0,
-#'   "border-radius-sm" = 0
-#' )
-#' preview_button(bootstrap())
-#'
-#' # If you need to set a variable based on another Bootstrap variable
-#' bs_theme_add_variables(
-#'   "body-color" = "$success",
-#'   .where = "declarations"
-#' )
-#' preview_button(bootstrap())
-#'
-#' # Start a new global theme and add some custom rules that
-#' # use Bootstrap variables to define a custom styling for a
-#' # 'person card'
+#' # Use latest Bootstrap version, default theme
 #' bs_theme_new()
-#' bs_theme_add(
-#'   rules = sass::sass_file(
-#'     system.file("custom", "person.scss", package = "bootstraplib")
-#'   )
+#'
+#' # When a theme is set, bs_theme_get() returns all the Sass code
+#' # necessary to compile Bootstrap, plus any theming customizations.
+#' class(bs_theme_get())
+#'
+#' # If you just want to know the
+#' theme_version()
+#'
+#' # Preview the current theme styling in a shiny app
+#' if (interactive()) bs_theme_preview()
+#'
+#' # Latest Bootstrap version, minty theme
+#' bs_theme_new(bootswatch = "minty")
+#' theme_bootswatch()
+#' if (interactive()) bs_theme_preview()
+#'
+#' # Use Bootstrap 3 with theme customization
+#' bs_theme_new(3)
+#' bs_theme_base_colors(bg = "#444", fg = "#e4e4e4")
+#' bs_theme_accent_colors(primary = "#e39777")
+#' if (interactive()) bs_theme_preview(with_themer = FALSE)
+#'
+#' # Since themes are sass layers, you can work with them
+#' # locally as if they were any other Sass layer
+#' # (i.e. you don't have to modify global state to add theme customizations)
+#' bs_theme_new("4+3")
+#' theme <- sass::sass_layer_merge(
+#'   bs_theme_get(),
+#'   sass::sass_layer(defaults = list("primary" = "red !default"))
 #' )
-#' # Include custom CSS that leverages bootstrap Sass variables
-#' person <- function(name, title, company) {
-#'   tags$div(
-#'     class = "person",
-#'     h3(class = "name", name),
-#'     div(class = "title", title),
-#'     div(class = "company", company)
-#'   )
-#' }
-#' browsable(tags$body(
-#'   bootstrap(),
-#'   person("Andrew Carnegie", "Owner", "Carnegie Steel Company"),
-#'   person("John D. Rockefeller", "Chairman", "Standard Oil")
-#' ))
-#'
-#'
-#' # Once a theme has been set, you can get it, and see which
-#' # version/bootswatch was specified
-#' bs_theme_new(bootswatch = "cosmo")
-#' theme <- bs_theme_get()
-#' theme_version(theme)
-#' theme_bootswatch(theme)
-#'
-#' # Themes are just sass_layer(), so you can work with them locally
-#' # just like any other sass layer
-#' # https://rstudio.github.io/sass/articles/sass.html#layers
-#' class(theme)
-#' layer <- sass::sass_layer("$primary: red")
-#' theme <- sass::sass_layer_merge(theme, layer)
-#' bs_theme_set(theme)
+#' bootstrap_sass(
+#'   ".my-class { color: mix($primary, $secondary, 50%); }",
+#'   theme
+#' )
 #'
 bs_theme_new <- function(version = version_default(), bootswatch = NULL) {
   theme <- bs_theme_create(version)
   theme <- bs_theme_add_bootswatch(theme, version, bootswatch)
   bs_theme_set(theme)
 }
-
-
-#' @rdname theming
-#' @export
-bs_theme_add_variables <- function(..., .where = "defaults",
-                                   .default_flag = identical(.where, "defaults")) {
-  vars <- rlang::list2(...)
-  if (any(names2(vars) == "")) stop("Variables must be named.", call. = FALSE)
-  .where <- match.arg(.where, c("defaults", "declarations", "rules"))
-
-  # Workaround to the problem of 'blue' winning in the scenario of:
-  # bs_theme_add_variables("body-bg" = "blue")
-  # bs_theme_add_variables("body-bg" = "red")
-  if (.default_flag) {
-    vars <- ensure_default_flag(vars)
-  }
-
-  do.call(bs_theme_add, setNames(list(vars), .where))
-}
-
-# Given a named list of variable definitions,
-# searches each variable's expression for a !default flag,
-# and if missing, adds it.
-ensure_default_flag <- function(vars) {
-  Map(
-    function(key, val) {
-      val <- paste(sass::as_sass(val), collapse = "\n")
-      if (grepl("!default\\s*;*\\s*$", val)) {
-        val
-      } else {
-        paste(sub(";+$", "", val), "!default")
-      }
-    }, names(vars), vars
-  )
-}
-
-#' @rdname theming
-#' @export
-bs_theme_add <- function(defaults = "", declarations = "", rules = "", ...) {
-  old_theme <- bs_theme_get()
-  if (is.null(old_theme)) {
-    stop("Must call bs_theme_new() before adding to the theme.", call. = FALSE)
-  }
-  if (inherits(defaults, "sass_layer")) {
-    if (!identical(declarations, "") || !identical(rules, "") || length(list(...)) > 0) {
-      stop(
-        "Not allowed to specify other arguments when the first argument, `defaults`, ",
-        "is a sass_layer()", call. = FALSE
-      )
-    }
-    layer <- defaults
-  } else {
-    layer <- sass_layer(defaults = defaults, declarations = declarations, rules = rules, ...)
-  }
-  layer <- sass_layer_merge(old_theme, layer)
-  bs_theme_set(add_class(layer, "bs_theme"))
-}
-
 
 #' @rdname theming
 #' @export
