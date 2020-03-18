10c81ef4da58136c379f43af28863af31f9b394b
Carson <cpsievert1@gmail.com>
2020-03-18 14:25:58 -0500

    Make it more obvious what the 'options' section does in the demo app

--- inst/themer-demo/app.R
+++ inst/themer-demo/app.R
@@ -14,6 +14,37 @@ tabPanel <- function(...) {
   #shiny::tabPanel(..., class = "p-3 border border-top-0 rounded-bottom")
 }
 
+gradient <- function(theme_color = "primary") {
+  bg_color <- paste0("bg-", theme_color)
+  bgg_color <- paste0("bg-gradient-", theme_color)
+  text_color <- if (theme_color == "light") "text-dark" else "text-light"
+  bg_div <- function(color_class, ...) {
+    div(
+      class = text_color, class = "p-3", class = color_class,
+      paste0(".", color_class), ...
+    )
+  }
+  fluidRow(
+    column(6, bg_div(bg_color)),
+    column(6, bg_div(bgg_color))
+  )
+}
+
+theme_colors <- c("primary", "secondary", "default", "success", "info", "warning", "danger", "dark")
+gradients <- lapply(theme_colors, gradient)
+
+progressBar <- div(
+  class="progress",
+  div(
+    class="progress-bar w-25",
+    role="progressbar",
+    "aria-valuenow"="25",
+    "aria-valuemin"="0",
+    "aria-valuemax"="100"
+  )
+)
+
+
 # TODO: Add pills, renderPlot() autocolors, more inputs, progress, etc
 ui <- navbarPage(
   header = bootstraplib::bootstrap(),
@@ -42,18 +73,48 @@ ui <- navbarPage(
         checkboxGroupInput("checkboxGroup", "checkboxGroupInput():", c("A", "B", "C"))
       ),
       mainPanel(
-        tabsetPanel(type= "pills",
+        tabsetPanel(
+          type= "pills",
           tabPanel("Tables", DT::dataTableOutput("dt_table")),
           tabPanel("Plots", plotOutput("ggplot")),
           tabPanel(
             "Typography",
-              h4("Verbatim text output"),
-              verbatimTextOutput("txtout"),
-              h1("Header 1"),
-              h2("Header 2"),
-              h3("Header 3"),
-              h4("Header 4"),
-              h5("Header 5")
+            h4("Verbatim text output"),
+            verbatimTextOutput("txtout"),
+            h1("Header 1"),
+            h2("Header 2"),
+            h3("Header 3"),
+            h4("Header 4"),
+            h5("Header 5")
+          ),
+          tabPanel(
+            "Options",
+            p(
+              "Background color gradients are disabled by default.",
+              "Enable them to see the difference here.",
+              "If enabled, gradients automatically apply buttons and progress bars, ",
+              "but you may also add to a .bg-gradient-* modified class to arbitrary elements."
+            ),
+            !!!gradients,
+            br(),
+            p(
+              "With the default settings, enabling of box shadows adds a very subtle and
+              barely noticable inner box-shadow to most input widgets. The difference is
+              a little more obvious in a progress bar:"
+            ),
+            progressBar,
+            br(),
+            p(
+              "Rounded corners are enabled by default and apply to numerous components (e.g., ",
+              code("tabsetPanel()"), ",", code("wellPanel()"), ", and ", code("actionButton()"), "):",
+              actionButton("progress", "Show progress")
+            ),
+            br(),
+            p(
+              "Smooth transitions are enabled by default, but if you disable them, progress updating",
+              "in ", code("fileInput()"), "and ", code("Progress"), " will appear more staggered",
+              "(e.g., click button above)."
+            )
           )
         )
       )
@@ -75,5 +136,19 @@ shinyApp(
         ggtitle("Diamond price by carat and cut")
     })
     output$txtout <- renderPrint(input$txt)
+    observeEvent(input$progress, {
+      withProgress(
+        message = "Here's a progress bar",
+        detail = "Disable the transition option for 'staggered' progress.",
+        value = 0,
+        {
+          for (i in 1:15) {
+            incProgress(1/15)
+            Sys.sleep(0.25)
+          }
+        }
+      )
+    })
+
   }
 )
