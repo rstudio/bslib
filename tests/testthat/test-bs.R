be0fb8e9bf5701efe8d401abbe741baf4dcf5e41
Carson Sievert <cpsievert1@gmail.com>
2020-09-11 18:51:24 -0500

    Bootswatch local fonts (#96)
    * Add accessibility layer for BS3, automate updating of bootstrap-colorpicker, and ensure lib starts from stratch when updating. Closes #80
    
    * Update tools/fonts.R to support any Google Fonts API version (& remove google-font-download dependency)
    
    * code review
    
    * update test expectations to reflect 4.5.2 changes

--- tests/testthat/test-bs.R
+++ tests/testthat/test-bs.R
@@ -3,7 +3,7 @@ context("bootswatch")
 
 test_that("Using bootswatch arg is equivalent to theme_layer_bootswatch()", {
   cosmo_primary <- bootstrap_sass("body{color:$primary;}", theme = "cosmo@4")
-  expect_css("body{color:#2780E3;}", cosmo_primary)
+  expect_css("body{color:#2780e3;}", cosmo_primary)
   # Some downstream dependencies will use $navbar-height
   paper_navbar <- bootstrap_sass("body{padding-top:$navbar-height;}", theme = "paper@4+3")
   # make sure it's different from the default
