aad7484964dfc7e12d4572afa018397df2082c2c
Barret Schloerke <schloerke@gmail.com>
2020-09-28 11:46:17 -0400

    Update test-utils.R

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,5 +2,11 @@ test_that("lib_file returns multiple system files", {
 
   # version 4 has a js file and a js map file
   js_files <- bootstrap_javascript("4")
-  expect_equal(length(js_files), 2)
+  expect_equal(length(js_files), 1)
+  js_files <- bootstrap_javascript("3")
+  expect_equal(length(js_files), 1)
+  js_files <- bootstrap_javascript_map("4")
+  expect_equal(length(js_files), 1)
+  js_files <- bootstrap_javascript_map("3")
+  expect_equal(length(js_files), 0)
 })
