a64ddf5fb2c988c1550aeb9ec8cc973e7adb53c1
Barret Schloerke <schloerke@gmail.com>
2020-10-26 10:36:18 -0400

    Let sass perform auto upgrading. No need to verify values

--- R/bs-theme-layers.R
+++ R/bs-theme-layers.R
@@ -123,9 +123,6 @@ bs_add_declarations <- function(theme, declarations) {
 #' @export
 bs_bundle <- function(theme, ...) {
   assert_bs_theme(theme)
-  is_layer <- vapply(rlang::list2(...), is_sass_bundle, logical(1))
-  if (!any(is_layer)) {
-    stop("`...` must only contain `sass::sass_layer()` or `sass::sass_bundle()` object(s)")
-  }
+
   add_class(sass_bundle(theme, ...), "bs_theme")
 }
