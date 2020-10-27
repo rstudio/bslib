806f49368e3d03eabaa2e0073e200c63667f5b5e
Carson Sievert <cpsievert1@gmail.com>
2020-10-27 15:53:51 -0500

    Make sure bs_theme_dependencies() retrieves HTML dependencies from theme (follow up from #122) (#138)

--- tests/testthat/test-theme-version.R
+++ tests/testthat/test-theme-version.R
@@ -9,4 +9,10 @@ test_that("Can retrieve version from theme object", {
   expect_identical(
     theme_version(bs_theme(version = "4")), "4"
   )
+  expect_identical(
+    theme_version(bs_theme(version = "4+3")), "4+3"
+  )
+  deps <- bs_theme_dependencies(bs_theme(version = "4+3"))
+  # jQuery, Bootstrap, and bs3compat layer
+  expect_length(deps, 3)
 })
