c1b37ff7a88b68dcdd3d715e6eedf24e0c1a7397
Carson <cpsievert1@gmail.com>
2020-11-13 18:33:39 -0600

    Unit tests

--- tests/testthat/test-theme-base-colors.R
+++ tests/testthat/test-theme-base-colors.R
@@ -227,4 +227,16 @@ test_that("theme-color('default') works as expected", {
     ".foo{color:theme-color('default')}", bs_theme(version = 4) %>% bs_remove("bs3compat")
   )
   expect_css(css, "")
+  expect_error(
+    sass::sass(
+      list(bs_theme(), ".foo { @extend .bg-default; }")
+    ),
+    NA
+  )
+  expect_error(
+    sass::sass(
+      list(bs_theme() %>% bs_remove("bs3compat"), ".foo { @extend .bg-default; }")
+    ),
+    "bg-default"
+  )
 })
