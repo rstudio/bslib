0989d2d5fbf8c1635e128e1e069f286100b9a727
Carson <cpsievert1@gmail.com>
2020-11-18 17:25:16 -0600

    add docs; make sure we can perform a reactive read

--- R/bs-current-theme.R
+++ R/bs-current-theme.R
@@ -1,14 +1,37 @@
-#' Obtain the currently active theme
-#' 
-#' @param session 
+#' Obtain the currently active theme at render time
+#'
+#' Intended for advanced use by developers to obtain the currently active theme
+#' _at render time_ and primarily for implementing themable widgets that can't
+#' otherwise be themed via [bs_dependency_defer()] .
+#'
+#' @details
+#'
+#' This function should generally only be called at print/render time. For
+#' example:
+#'
+#'  * Inside the `preRenderHook` of [htmlwidgets::createWidget()].
+#'  * Inside of a custom [print] method that generates [htmltools::tags].
+#'  * Inside of a [htmltools::tagFunction()]
+#'
+#' Calling this function at print/render time is important because it does
+#' different things based on the context in which it's called:
+#'
+#' * If a reactive context is active, `session$getCurrentTheme()` is called
+#'   (which is a reactive read).
+#' * If no reactive context is active, `shiny::getCurrentTheme()` is called
+#'   (which returns the current app's `theme`, if relevant).
+#' * If `shiny::getCurrentTheme()` comes up empty, then `bs_global_get()`
+#'   is called, which is relevant for [rmarkdown::html_document()], and
+#'   possibly other static rendering contexts.
+#'
+#' @param session The current Shiny session (if any).
 #'
 #' @return a [bs_theme()] object.
 #' @export
-#'
-#' @examples
 bs_current_theme <- function(session = shiny::getDefaultReactiveDomain()) {
-  # If called in a reactive context, this'll be a reactive read
-  if (!is.null(session) && shiny:::hasCurrentContext()) {
+  # If we're able to make a reactive read of the theme, then do it
+  hasReactiveContext <- getFromNamespace("hasCurrentContext", "shiny") %||% function() FALSE
+  if (!is.null(session) && "getCurrentTheme" %in% names(session) && hasReactiveContext()) {
     return(session$getCurrentTheme())
   }
   # Otherwise, this'll be a non-reactive read of session/app/global state
