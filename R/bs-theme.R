561f4d46eb2b37f729427db67439a24a46776f4b
Carson <cpsievert1@gmail.com>
2021-04-28 12:56:07 -0500

    Also allow for 'bootstrap'

--- R/bs-theme.R
+++ R/bs-theme.R
@@ -374,7 +374,9 @@ bs3_accessibility_bundle <- function() {
 # -----------------------------------------------------------------
 
 bootswatch_bundle <- function(bootswatch, version) {
-  if (!length(bootswatch) || identical(bootswatch, "default")) return(NULL)
+  if (!length(bootswatch) || isTRUE(bootswatch %in% c("default", "bootstrap"))) {
+    return(NULL)
+  }
 
   bootswatch <- switch_version(
     version,
