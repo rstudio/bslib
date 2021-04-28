3fd3797cae76ac1bad45793df0f58a86c70ad333
Carson Sievert <cpsievert1@gmail.com>
2021-04-28 13:15:33 -0500

    Allow for bootswatch='default' in bs_theme() (#308)
    * Allow for bootswatch='default' in bs_theme() as a way to opt out of a different default Bootswatch theme
    
    (Will be useful for flexdashboard::flex_dashboard, which wants to use Bootswatch cosmo by default
    
    * Also allow for 'bootstrap'
    
    * Update tests/testthat/test-theme-bootswatch.R

--- tests/testthat/test-theme-bootswatch.R
+++ tests/testthat/test-theme-bootswatch.R
@@ -15,4 +15,15 @@ test_that("Can retrieve version from theme object", {
   expect_identical(
     theme_bootswatch(theme), "darkly"
   )
+
+  # Can use default to effectively remove bootswatch
+  expect_identical(
+    sass::sass(bs_theme_update(theme, bootswatch = "default")),
+    sass::sass(bs_theme(version = "4"))
+  )
+
+  # Can use default as a way to "explicitly don't use" a Bootswatch theme
+  default <- bs_theme(version = "4", bootswatch = "default")
+  expect_equal(default, bs_theme(version = "4"), ignore_attr = TRUE)
+  expect_equal(theme_bootswatch(default), "default")
 })
