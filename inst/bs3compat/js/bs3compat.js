6b1575fbfb1c81be464360768bd30095c13404d8
Carson <cpsievert1@gmail.com>
2021-06-02 17:05:33 -0500

    Fix tab shim for Bootstrap 5 case

--- inst/bs3compat/js/bs3compat.js
+++ inst/bs3compat/js/bs3compat.js
@@ -2,24 +2,20 @@
 window.BS3_COMPAT = true;
 
 (function($) {
-  // For Bootstrap 5+, we don't need to overwrite the Tab constructor
-  // (BS3 markup 'just works' since tabs.js is sourced and the
-  //  BS5 logic will only be triggered by data-bs-toggle='tab')
-  if (window.bootstrap && window.bootstrap.Tab) {
-    return;
-  }
 
+  // We always bundle Bootstrap 3's tab plugin (after the main Bootstrap JS bundle),
+  // so this should (in theory) always be true at this point
   if (!$.fn.tab.Constructor.VERSION.match(/^3\./)) {
     (console.warn || console.error || console.log)("bs3compat.js couldn't find bs3 tab impl; bs3 tabs will not be properly supported");
     return;
   }
   var bs3TabPlugin = $.fn.tab.noConflict();
 
-  if (!$.fn.tab.Constructor.VERSION.match(/^4\./)) {
-    (console.warn || console.error || console.log)("bs3compat.js couldn't find bs4 tab impl; bs3 tabs will not be properly supported");
-    return;
+  // Bootstrap 5 removed jQuery and thus has removed the plugin
+  var bs4TabPlugin = null;
+  if ($.fn.tab && $.fn.tab.noConflict) {
+    bs4TabPlugin = $.fn.tab.noConflict();
   }
-  var bs4TabPlugin = $.fn.tab.noConflict();
 
   var EVENT_KEY = "click.bs.tab.data-api";
   var SELECTOR = '[data-toggle="tab"], [data-toggle="pill"]';
@@ -34,19 +30,27 @@ window.BS3_COMPAT = true;
     if ($(this).closest(".nav").find(".nav-item, .nav-link").length === 0) {
       // Bootstrap 3 tabs detected
       bs3TabPlugin.call($(this), config);
-    } else {
+    } else if (bs4TabPlugin) {
       // Bootstrap 4 tabs detected
       bs4TabPlugin.call($(this), config);
+    } else {
+      // In the Bootstrap 5+ case, do nothing since the data-bs-toggle namespace
+      // will trigger it's own events
     }
   }
 
+  // Register our plugin shim
   var noconflict = $.fn.tab;
   $.fn.tab = TabPlugin;
-  $.fn.tab.Constructor = bs4TabPlugin.Constructor;
-  $.fn.tab.noConflict = function() {
-    $.fn.tab = noconflict;
-    return TabPlugin;
-  };
+
+  // TODO: do we need to do this for BS5?
+  if (bs4TabPlugin) {
+    $.fn.tab.Constructor = bs4TabPlugin.Constructor;
+    $.fn.tab.noConflict = function() {
+      $.fn.tab = noconflict;
+      return TabPlugin;
+    };
+  }
 
 })(jQuery);
 
