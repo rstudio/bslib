7971fc4fa4ff3f2c76b5d6a6ae2fddae2c62d126
Barret Schloerke <schloerke@gmail.com>
2021-11-04 15:15:31 -0400

    Install packages from the themer demo

--- .github/shinycoreci-steps/__update_website_deps.R
+++ .github/shinycoreci-steps/__update_website_deps.R
@@ -1,13 +1,20 @@
+demo_pkgs <- unique(renv::dependencies("inst/themer-demo", progress = FALSE)$Package)
 vig_pkgs <- unique(renv::dependencies("vignettes", progress = FALSE)$Package)
+
 
 known_pkgs <- c(
     "rstudio/quillt",
     "BH", # it is not picked up for some reason
-    vig_pkgs
+    vig_pkgs,
+    demo_pkgs
 )
 
+installed_packages <- as.data.frame(installed.packages())
+base_pkgs <- unname(unlist(subset(installed_packages, Priority == "base", "Package")))
+
 desc_pkgs <- desc::desc_get_deps()$package
-pkgs <- setdiff(known_pkgs, c("R", "bslib", desc_pkgs))
+
+pkgs <- sort(setdiff(known_pkgs, c("R", "bslib", desc_pkgs, base_pkgs)))
 
 pkg_txt <- paste0(paste0("\n    ", pkgs), collapse = "")
 desc::desc_set("Config/Needs/website", pkg_txt)
