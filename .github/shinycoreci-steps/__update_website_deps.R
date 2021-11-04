cc24a62d90ce2fe1239555f5f3530d6de593ee54
Barret Schloerke <barret@rstudio.com>
2021-11-04 16:46:08 -0400

    master -> main (#403)
    Co-authored-by: schloerke <schloerke@users.noreply.github.com>

---
+++ .github/shinycoreci-steps/__update_website_deps.R
@@ -0,0 +1,20 @@
+
+installed_packages <- as.data.frame(installed.packages())
+base_pkgs <- unname(unlist(subset(installed_packages, Priority == "base", "Package")))
+desc_pkgs <- desc::desc_get_deps()$package
+
+set_desc_with_packages <- function(field, known_pkgs) {
+
+  pkgs <- sort(setdiff(known_pkgs, c("R", "bslib", desc_pkgs, base_pkgs)))
+
+  pkg_txt <- paste0(paste0("\n    ", pkgs), collapse = ",")
+  desc::desc_set(paste0("Config/Needs/", field), pkg_txt)
+}
+
+
+demo_pkgs <- unique(renv::dependencies("inst/themer-demo", progress = FALSE)$Package)
+vig_pkgs <- unique(renv::dependencies("vignettes", progress = FALSE)$Package)
+
+set_desc_with_packages("website", c("rstudio/quillt", vig_pkgs))
+# BH is not picked up for some reason
+set_desc_with_packages("deploy", c("BH", demo_pkgs))
