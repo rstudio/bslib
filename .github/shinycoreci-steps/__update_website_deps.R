2285ef07d8886d7cc3826b967d66ac2e2aebc664
Barret Schloerke <schloerke@gmail.com>
2021-11-04 15:53:25 -0400

    Use two separate fields for pkg deps for different GHA jobs

--- .github/shinycoreci-steps/__update_website_deps.R
+++ .github/shinycoreci-steps/__update_website_deps.R
@@ -1,20 +1,20 @@
-demo_pkgs <- unique(renv::dependencies("inst/themer-demo", progress = FALSE)$Package)
-vig_pkgs <- unique(renv::dependencies("vignettes", progress = FALSE)$Package)
-
-
-known_pkgs <- c(
-    "rstudio/quillt",
-    "BH", # it is not picked up for some reason
-    vig_pkgs,
-    demo_pkgs
-)
 
 installed_packages <- as.data.frame(installed.packages())
 base_pkgs <- unname(unlist(subset(installed_packages, Priority == "base", "Package")))
-
 desc_pkgs <- desc::desc_get_deps()$package
 
-pkgs <- sort(setdiff(known_pkgs, c("R", "bslib", desc_pkgs, base_pkgs)))
+set_desc_with_packages <- function(field, known_pkgs) {
 
-pkg_txt <- paste0(paste0("\n    ", pkgs), collapse = ",")
-desc::desc_set("Config/Needs/website", pkg_txt)
+  pkgs <- sort(setdiff(known_pkgs, c("R", "bslib", desc_pkgs, base_pkgs)))
+
+  pkg_txt <- paste0(paste0("\n    ", pkgs), collapse = ",")
+  desc::desc_set(paste0("Config/Needs/", field), pkg_txt)
+}
+
+
+demo_pkgs <- unique(renv::dependencies("inst/themer-demo", progress = FALSE)$Package)
+vig_pkgs <- unique(renv::dependencies("vignettes", progress = FALSE)$Package)
+
+set_desc_with_packages("website", c("rstudio/quillt", vig_pkgs))
+# BH is not picked up for some reason
+set_desc_with_packages("deploy", c("BH", demo_pkgs))
