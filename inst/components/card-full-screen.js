98580b646ab6796692493d0f8ebf9d4fca359b5b
gregswinehart <>
2022-07-22 12:18:48 -0700

    design tweaks to Close Fullscreen view

--- inst/components/card-full-screen.js
+++ inst/components/card-full-screen.js
@@ -76,7 +76,7 @@ function enterFullScreen(card) {
 
   // Add an overlay behind the card
   // (assumes the fontawesome dependency is already available)
-  const overlay = $("<div id='bslib-full-screen-overlay'><a class='bslib-full-screen-exit'>Close<i class='fa fa-window-close'></i></a></div>");
+  const overlay = $("<div id='bslib-full-screen-overlay'><a class='bslib-full-screen-exit'>Close <svg width:'20' height='20' fill='currentColor' class='bi bi-x-lg' viewBox='0 0 16 16'><path d='M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z'/></svg></a></div>");
   card.insertAdjacentElement("beforebegin", overlay[0]);
 }
 
