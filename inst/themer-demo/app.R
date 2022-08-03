fa2e03cd4942a828b21733e2a022c492b3032690
Carson <cpsievert1@gmail.com>
2022-08-03 14:14:09 -0500

    Better fontawesome 0.3 support in bs_themer()

--- inst/themer-demo/app.R
+++ inst/themer-demo/app.R
@@ -94,7 +94,11 @@ shinyApp(
             actionButton("success", "Success", icon("check"), class = "btn-success m-2"),
             actionButton("info", "Info", icon("info"),  class = "btn-info m-2"),
             actionButton("warning", "warning", icon("exclamation"), class = "btn-warning m-2"),
-            actionButton("danger", "Danger", icon("exclamation-triangle"), class = "btn-danger m-2"),
+            actionButton(
+              "danger", "Danger",
+              icon(if (packageVersion("fontawesome") >= "0.3") "triangle-exclamation" else "exclamation-triangle"),
+              class = "btn-danger m-2"
+            ),
             actionButton("dark", "Dark", icon("moon"), class = "btn-dark m-2"),
             actionButton("light", "Light", icon("sun"), class = "btn-light m-2")
           )
