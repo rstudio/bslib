7a42ad1023267952c5573f91e3c1e3a008a83e0d
Carson <cpsievert1@gmail.com>
2023-01-11 16:23:26 -0600

    Run eslint and tsc checks before yarn build

--- srcts/src/components/_utils.ts
+++ srcts/src/components/_utils.ts
@@ -1,15 +1,29 @@
+import type { HtmlDep } from "rstudio-shiny/srcts/types/src/shiny/render";
 
-import { HtmlDep } from "rstudio-shiny/srcts/types/src/shiny/render";
-
-// TODO: Is there a way to get an actual type for Shiny.InputBinding without having to import it?
 import type { InputBinding as InputBindingType } from "rstudio-shiny/srcts/types/src/bindings/input";
-const InputBinding = (window.Shiny ? Shiny.InputBinding : class {}) as typeof InputBindingType;
 
-function registerBinding(inputBindingClass: new () => InputBindingType, name: string) {
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const InputBinding = (
+  window.Shiny ? Shiny.InputBinding : class {}
+) as typeof InputBindingType;
+
+function registerBinding(
+  inputBindingClass: new () => InputBindingType,
+  name: string
+): void {
   if (window.Shiny) {
     Shiny.inputBindings.register(new inputBindingClass(), "bslib." + name);
   }
 }
 
-export { InputBinding, registerBinding };
+// Inspiration from https://fettblog.eu/typescript-hasownproperty/
+// But mixing with "NonNullable key of Obj" instead of "key to unknown values"
+function hasOwnProperty<Prop extends keyof X, X extends { [key: string]: any }>(
+  obj: X,
+  prop: Prop
+): obj is X & { [key in NonNullable<Prop>]: X[key] } {
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}
+
+export { InputBinding, registerBinding, hasOwnProperty };
 export type { HtmlDep };
