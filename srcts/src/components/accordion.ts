2c3b5a5815f3acfba18dca939cf0421a4bdec55d
Carson <cpsievert1@gmail.com>
2023-01-11 16:23:26 -0600

    Run eslint and tsc checks before yarn build

--- srcts/src/components/accordion.ts
+++ srcts/src/components/accordion.ts
@@ -1,72 +1,78 @@
-import { InputBinding, registerBinding, HtmlDep } from "./_utils";
+import type { HtmlDep } from "./_utils";
+import { InputBinding, registerBinding, hasOwnProperty } from "./_utils";
 
 type AccordionItem = {
-  item: Element,
-  value: string,
-  isOpen: () => boolean,
-  show: () => void,
-  hide: () => void,
+  item: Element;
+  value: string;
+  isOpen: () => boolean;
+  show: () => void;
+  hide: () => void;
 };
 
 type HTMLContent = {
-  html: string,
-  deps?: HtmlDep[]
+  html: string;
+  deps?: HtmlDep[];
 };
 
 type SetMessage = {
-  method: "set",
-  values: string[]
+  method: "set";
+  values: string[];
 };
 
 type OpenMessage = {
-  method: "open",
-  values: string[] | true
+  method: "open";
+  values: string[] | true;
 };
 
 type CloseMessage = {
-  method: "close",
-  values: string[] | true
+  method: "close";
+  values: string[] | true;
 };
 
 type InsertMessage = {
-  method: "insert",
-  panel: HTMLContent,
-  target: string,
-  position: "after" | "before",
+  method: "insert";
+  panel: HTMLContent;
+  target: string;
+  position: "after" | "before";
 };
 
-type RemoveMessage =  { 
-  method: "remove",
-  target: string[] 
-}
+type RemoveMessage = {
+  method: "remove";
+  target: string[];
+};
 
 type UpdateMessage = {
-  method: "update",
-  target: string,
-  value: string,
-  body: HTMLContent,
-  title: HTMLContent,
-  icon: HTMLContent,
-}
+  method: "update";
+  target: string;
+  value: string;
+  body: HTMLContent;
+  title: HTMLContent;
+  icon: HTMLContent;
+};
 
-type MessageData = SetMessage | OpenMessage | CloseMessage | InsertMessage | RemoveMessage | UpdateMessage;
+type MessageData =
+  | CloseMessage
+  | InsertMessage
+  | OpenMessage
+  | RemoveMessage
+  | SetMessage
+  | UpdateMessage;
 
-
-class accordionInputBinding extends InputBinding {
-
+class AccordionInputBinding extends InputBinding {
   find(scope: HTMLElement) {
     return $(scope).find(".accordion.bslib-accordion-input");
   }
 
   getValue(el: HTMLElement): string[] | null {
-    const items = this._getItemInfo(el)
-    const selected = items.filter(x => x.isOpen()).map(x => x.value);
+    const items = this._getItemInfo(el);
+    const selected = items.filter((x) => x.isOpen()).map((x) => x.value);
     return selected.length === 0 ? null : selected;
   }
 
   subscribe(el: HTMLElement, callback: (x: boolean) => void) {
     $(el).on(
       "shown.bs.collapse.accordionInputBinding hidden.bs.collapse.accordionInputBinding",
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       function (event) {
         callback(true);
       }
@@ -97,27 +103,27 @@ class accordionInputBinding extends InputBinding {
   }
 
   protected _setItems(el: HTMLElement, data: SetMessage) {
-    const items = this._getItemInfo(el)
+    const items = this._getItemInfo(el);
     const vals = this._getValues(el, items, data.values);
-    items.forEach(x => {
+    items.forEach((x) => {
       vals.indexOf(x.value) > -1 ? x.show() : x.hide();
-    })
+    });
   }
 
   protected _openItems(el: HTMLElement, data: OpenMessage) {
-    const items = this._getItemInfo(el)
+    const items = this._getItemInfo(el);
     const vals = this._getValues(el, items, data.values);
-    items.forEach(x => {
+    items.forEach((x) => {
       if (vals.indexOf(x.value) > -1) x.show();
-    })
+    });
   }
 
   protected _closeItems(el: HTMLElement, data: CloseMessage) {
-    const items = this._getItemInfo(el)
+    const items = this._getItemInfo(el);
     const vals = this._getValues(el, items, data.values);
-    items.forEach(x => {
+    items.forEach((x) => {
       if (vals.indexOf(x.value) > -1) x.hide();
-    })
+    });
   }
 
   protected _insertItem(el: HTMLElement, data: InsertMessage) {
@@ -126,15 +132,18 @@ class accordionInputBinding extends InputBinding {
     // If no target was specified, or the target was not found, then default
     // to the first or last item, depending on the position
     if (!targetItem) {
-      targetItem = (data.position === "before" ? el.firstElementChild : el.lastElementChild) as HTMLElement;
+      targetItem = (
+        data.position === "before" ? el.firstElementChild : el.lastElementChild
+      ) as HTMLElement;
     }
 
     const panel = data.panel;
-    
+
     // If there is still no targetItem, then there are no items in the accordion
     if (targetItem) {
       Shiny.renderContent(
-        targetItem, panel,
+        targetItem,
+        panel,
         data.position === "before" ? "beforeBegin" : "afterEnd"
       );
     } else {
@@ -151,63 +160,75 @@ class accordionInputBinding extends InputBinding {
   }
 
   protected _removeItem(el: HTMLElement, data: RemoveMessage) {
-    const targetItems = this._getItemInfo(el).filter(x => data.target.indexOf(x.value) > -1);
+    const targetItems = this._getItemInfo(el).filter(
+      (x) => data.target.indexOf(x.value) > -1
+    );
 
-    targetItems.forEach(x => x.item.remove());
+    targetItems.forEach((x) => x.item.remove());
   }
 
   protected _updateItem(el: HTMLElement, data: UpdateMessage) {
     const target = this._findItem(el, data.target);
 
     if (!target) {
-      throw new Error(`Unable to find an accordion_panel() with a value of ${data.target}`);
+      throw new Error(
+        `Unable to find an accordion_panel() with a value of ${data.target}`
+      );
     }
 
-    if (data.hasOwnProperty("value")) {
+    if (hasOwnProperty(data, "value")) {
       target.dataset.value = data.value;
     }
 
-    if (data.hasOwnProperty("body")) {
+    if (hasOwnProperty(data, "body")) {
       const body = target.querySelector(".accordion-body") as HTMLElement; // always exists
       Shiny.renderContent(body, data.body);
     }
 
     const header = target.querySelector(".accordion-header") as HTMLElement; // always exists
 
-    if (data.hasOwnProperty("title")) {
+    if (hasOwnProperty(data, "title")) {
       const title = header.querySelector(".accordion-title") as HTMLElement; // always exists
       Shiny.renderContent(title, data.title);
     }
 
-    if (data.hasOwnProperty("icon")) {
-      const icon = header.querySelector(".accordion-button > .accordion-icon") as HTMLElement; // always exists
+    if (hasOwnProperty(data, "icon")) {
+      const icon = header.querySelector(
+        ".accordion-button > .accordion-icon"
+      ) as HTMLElement; // always exists
       Shiny.renderContent(icon, data.icon);
     }
   }
 
   protected _getItemInfo(el: HTMLElement): AccordionItem[] {
-    const items = Array.from(el.querySelectorAll(":scope > .accordion-item")) as HTMLElement[];
-    return items.map(x => this._getSingleItemInfo(x));
+    const items = Array.from(
+      el.querySelectorAll(":scope > .accordion-item")
+    ) as HTMLElement[];
+    return items.map((x) => this._getSingleItemInfo(x));
   }
 
   protected _getSingleItemInfo(x: HTMLElement): AccordionItem {
     const collapse = x.querySelector(".accordion-collapse") as HTMLElement;
-    const isOpen = () => $(collapse).hasClass("show")
+    const isOpen = () => $(collapse).hasClass("show");
     return {
       item: x,
       value: x.dataset.value as string,
       isOpen: isOpen,
       show: () => {
-        if (!isOpen()) $(collapse).collapse("show")
+        if (!isOpen()) $(collapse).collapse("show");
       },
       hide: () => {
-        if (isOpen()) $(collapse).collapse("hide")
-      }
+        if (isOpen()) $(collapse).collapse("hide");
+      },
     };
   }
 
-  protected _getValues(el: HTMLElement, items: AccordionItem[], values: string[] | true): string[] {
-    let vals = values !== true ? values : items.map(x => x.value);
+  protected _getValues(
+    el: HTMLElement,
+    items: AccordionItem[],
+    values: string[] | true
+  ): string[] {
+    let vals = values !== true ? values : items.map((x) => x.value);
     const autoclose = this._isAutoClosing(el);
     if (autoclose) {
       vals = vals.slice(vals.length - 1, vals.length);
@@ -224,5 +245,4 @@ class accordionInputBinding extends InputBinding {
   }
 }
 
-
-registerBinding(accordionInputBinding, "accordion");
+registerBinding(AccordionInputBinding, "accordion");
