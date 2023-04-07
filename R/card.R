24b15cf05c98c0b02cb79713314aebbc43c263d1
Carson <cpsievert1@gmail.com>
2023-04-07 11:06:00 -0500

    Close #540: include card()'s init JS code even when full_creen=F

--- R/card.R
+++ R/card.R
@@ -68,7 +68,8 @@ card <- function(..., full_screen = FALSE, height = NULL, max_height = NULL, fil
     ),
     !!!attribs,
     !!!children,
-    if (full_screen) full_screen_toggle()
+    if (full_screen) full_screen_toggle(),
+    card_js_init()
   )
 
   tag <- bindFillRole(tag, container = TRUE, item = fill)
@@ -279,51 +280,54 @@ full_screen_toggle <- function() {
       package = "bslib",
       src = "components",
       script = "card-full-screen.js"
-    ),
-    tags$script("data-bslib-card-needs-init" = NA, HTML(
-      "
-        var thisScript = document.querySelector('script[data-bslib-card-needs-init]');
-        if (!thisScript) throw new Error('Failed to register card() resize observer');
-
-        thisScript.removeAttribute('data-bslib-card-needs-init');
-
-        var card = $(thisScript).parents('.card').last();
-        if (!card) throw new Error('Failed to register card() resize observer');
-
-        // Let Shiny know to trigger resize when the card size changes
-        // TODO: shiny could/should do this itself (rstudio/shiny#3682)
-        var resizeEvent = window.document.createEvent('UIEvents');
-        resizeEvent.initUIEvent('resize', true, false, window, 0);
-        var ro = new ResizeObserver(() => { window.dispatchEvent(resizeEvent); });
-        ro.observe(card[0]);
-
-        // Enable tooltips (for the expand icon)
-        var tooltipList = card[0].querySelectorAll('[data-bs-toggle=\"tooltip\"]');
-        tooltipList.forEach(function(x) { new bootstrap.Tooltip(x); });
-
-        // In some complex fill-based layouts with multiple outputs (e.g., plotly),
-        // shiny initializes with the correct sizing, but in-between the 1st and last
-        // renderValue(), the size of the output containers can change, meaning every
-        // output but the 1st gets initialized with the wrong size during their
-        // renderValue(); and then after the render phase, shiny won't know trigger a
-        // resize since all the widgets will return to their original size
-        // (and thus, Shiny thinks there isn't any resizing to do).
-        // We workaround that situation by manually triggering a resize on the binding
-        // when the output container changes (this way, if the size is different during
-        // the render phase, Shiny will know about it)
-        $(document).on('shiny:value', function(x) {
-          var el = x.binding.el;
-          if (card[0].contains(el) && !$(el).data('bslib-output-observer')) {
-            var roo = new ResizeObserver(x.binding.onResize);
-            roo.observe(el);
-            $(el).data('bslib-output-observer', true);
-          }
-        });
-        "
-    ))
+    )
   )
 }
 
+
+card_js_init <- function() {
+  tags$script("data-bslib-card-needs-init" = NA, HTML(
+    "
+      var thisScript = document.querySelector('script[data-bslib-card-needs-init]');
+      if (!thisScript) throw new Error('Failed to register card() resize observer');
+
+      thisScript.removeAttribute('data-bslib-card-needs-init');
+
+      var card = $(thisScript).parents('.card').last();
+      if (!card) throw new Error('Failed to register card() resize observer');
+
+      // Let Shiny know to trigger resize when the card size changes
+      // TODO: shiny could/should do this itself (rstudio/shiny#3682)
+      var resizeEvent = window.document.createEvent('UIEvents');
+      resizeEvent.initUIEvent('resize', true, false, window, 0);
+      var ro = new ResizeObserver(() => { window.dispatchEvent(resizeEvent); });
+      ro.observe(card[0]);
+
+      // Enable tooltips (for the expand icon)
+      var tooltipList = card[0].querySelectorAll('[data-bs-toggle=\"tooltip\"]');
+      tooltipList.forEach(function(x) { new bootstrap.Tooltip(x); });
+
+      // In some complex fill-based layouts with multiple outputs (e.g., plotly),
+      // shiny initializes with the correct sizing, but in-between the 1st and last
+      // renderValue(), the size of the output containers can change, meaning every
+      // output but the 1st gets initialized with the wrong size during their
+      // renderValue(); and then after the render phase, shiny won't know trigger a
+      // resize since all the widgets will return to their original size
+      // (and thus, Shiny thinks there isn't any resizing to do).
+      // We workaround that situation by manually triggering a resize on the binding
+      // when the output container changes (this way, if the size is different during
+      // the render phase, Shiny will know about it)
+      $(document).on('shiny:value', function(x) {
+        var el = x.binding.el;
+        if (card[0].contains(el) && !$(el).data('bslib-output-observer')) {
+          var roo = new ResizeObserver(x.binding.onResize);
+          roo.observe(el);
+          $(el).data('bslib-output-observer', true);
+        }
+      });
+    "
+  ))
+}
 
 # via bsicons::bs_icon("arrows-fullscreen")
 full_screen_toggle_icon <- function() {
