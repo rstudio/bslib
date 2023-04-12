ab2c23a7b48ec220776c1c59a97ef7c37285a8e9
Garrick Aden-Buie <garrick@adenbuie.com>
2023-04-12 11:55:41 -0400

    NA `border` and `border_radius` now error

--- tests/testthat/test-sidebar.R
+++ tests/testthat/test-sidebar.R
@@ -128,13 +128,10 @@ test_that("layout_sidebar() - errors with unexpected border, border_radius input
   expect_error(
     layout_sidebar(sidebar(), border_radius = c(TRUE, NA))
   )
-})
-
-test_that("layout_sidebar() - NA border, border_radius is treated like NULL", {
-  expect_silent(
+  expect_error(
     layout_sidebar(sidebar(), border = NA)
   )
-  expect_silent(
+  expect_error(
     layout_sidebar(sidebar(), border_radius = NA)
   )
 })
