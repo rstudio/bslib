3b22f76a83bb469311b6a78fd56b8c38f3521b8f
Garrick Aden-Buie <garrick@adenbuie.com>
2023-04-12 14:00:21 -0400

    don't shrink images (will happen with routine)

--- .github/shiny-workflows/routine.sh
+++ .github/shiny-workflows/routine.sh
@@ -6,8 +6,3 @@ git commit R/sysdata.rda -m 'Resave data (GitHub Action)' || echo "No updates to
 # Update `Config/Needs/website` deployment dependencies
 Rscript ./.github/shiny-workflows/__update_website_deps.R
 git commit DESCRIPTION -m 'Update website deps (GitHub Action)' || echo "No new website vignette dependencies to commit"
-
-# Shrink images in man/figures
-sudo apt-get install optipng
-optipng man/figures/*.png
-git commit man/figures/*.png -m "Shrink images in man/figures (Github Action)" || echo "No changes to man/figures/*.png"
