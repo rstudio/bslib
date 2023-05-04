ce041dd597a06ff2b2b42d088f9510c8aaa0a3e8
Garrick Aden-Buie <garrick@adenbuie.com>
2023-05-04 09:05:50 -0400

    Don't need to check length of `children`
    Empty lists are also dropped by htmltools::tags

--- R/layout.R
+++ R/layout.R
@@ -78,14 +78,12 @@ layout_column_wrap <- function(
   #   2. Allow for heights_equal="-cell", which useful for allowing contents to
   #      shrink but not grow (i.e., default flex behavior).
   children <- dropNulls(children)
-  children <- if (length(children) > 0) {
-    lapply(children, function(x) {
-      bindFillRole(
-        container = TRUE,
-        div(bindFillRole(div(x), container = fillable, item = TRUE))
-      )
-    })
-  }
+  children <- lapply(children, function(x) {
+    bindFillRole(
+      container = TRUE,
+      div(bindFillRole(div(x), container = fillable, item = TRUE))
+    )
+  })
 
   tag <- div(
     class = "bslib-column-wrap",
