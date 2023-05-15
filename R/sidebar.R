a1cdea35696f5dc0a722c53dc82ec2caf8c5631b
Garrick Aden-Buie <garrick@adenbuie.com>
2023-05-15 11:03:05 -0400

    Use `<aside>` for sidebar element

--- R/sidebar.R
+++ R/sidebar.R
@@ -135,9 +135,8 @@ sidebar <- function(
     }
 
   res <- list2(
-    tag = tags$div(
+    tag = tags$aside(
       id = id,
-      role = "complementary",
       class = c("sidebar", class),
       hidden = if (open == "closed") NA,
       tags$div(
