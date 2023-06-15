52058c52b36974dcd76c7a80559ca587b8af0487
Garrick Aden-Buie <garrick@adenbuie.com>
2023-06-15 16:04:03 -0400

    chore(sidebar): trigger bare "shown" and "hidden" events

--- srcts/src/components/sidebar.ts
+++ srcts/src/components/sidebar.ts
@@ -370,11 +370,7 @@ class Sidebar {
 
     // Trigger Shiny input and output binding events
     $(sidebar).trigger("toggleCollapse.sidebarInputBinding");
-    $(sidebar).trigger(
-      this.isClosed
-        ? "hidden.sendOutputHiddenState"
-        : "shown.sendOutputHiddenState"
-    );
+    $(sidebar).trigger(this.isClosed ? "hidden" : "shown");
   }
 }
 
