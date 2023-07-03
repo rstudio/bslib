2ce8eebf06910da5d0719c7a05b9860db5da8c0f
Carson Sievert <cpsievert1@gmail.com>
2023-07-03 12:47:19 -0500

    Save a Sass file with `bs_theme()`'s 'default' Sass code (#655)

--- tools/yarn_install.R
+++ tools/yarn_install.R
@@ -431,4 +431,20 @@ lapply(versions(), function(version) {
     dir.create(dest_dir)
   }
   file.copy(tmp_css, dest_dir)
+
+  # Also save the BS5+ Sass code used to generate the pre-compiled CSS.
+  # This is primarily here to help Quarto more easily replicate bs_theme()'s Sass.
+  if (version >= 5) {
+    theme_sass <- gsub(
+      paste0("@import \"", getwd(), "/"),
+      "@import \"",
+      as_sass(bs_theme(version))
+    )
+    writeLines(theme_sass, file.path(dest_dir, "bootstrap.scss"))
+    # Sanity check that we we can compile by moving file to home dir
+    file.copy(file.path(dest_dir, "bootstrap.scss"), "bootstrap.scss")
+    on.exit(unlink("bootstrap.scss"), add = TRUE)
+    testthat::expect_error(sass(sass_file("bootstrap.scss")), NA)
+  }
 })
+
