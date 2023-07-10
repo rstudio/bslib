03c6655cb3acf78d9e35e8cc96c643ea2bf808d4
Garrick Aden-Buie <garrick@adenbuie.com>
2023-07-10 17:07:04 -0400

    chore: Apply my suggestions

--- R/input-switch.R
+++ R/input-switch.R
@@ -1,12 +1,18 @@
 #' Switch input control
 #'
-#' Create a switch control used to specify logical values.
+#' Create an on-off style switch control for specifying logical values.
 #'
 #' @param id An input id.
 #' @param label A label for the switch.
 #' @param value Whether or not the switch should be checked by default.
 #' @param width Any valid [CSS unit][htmltools::validateCssUnit] (e.g.,
 #'   `width="200px"`).
+#'
+#' @return Returns a UI element for a switch input control. The server value
+#'   received for the input corresponding to `id` will be a logical
+#'   (`TRUE`/`FALSE`) value.
+#'
+#' @family input controls
 #' @export
 input_switch <- function(id, label, value = FALSE, width = NULL) {
   tag <- input_checkbox(id, label, class = "form-check form-switch", value = value, width = width)
@@ -24,11 +30,16 @@ update_switch <- function(id, label = NULL, value = NULL, session = get_current_
 
 input_checkbox <- function(id, label, class = "form-check", value = FALSE, width = NULL, inline = FALSE) {
   div(
+    class = "form-group shiny-input-container",
+    class = if (inline) "shiny-input-container-inline",
+    style = css(width = width),
     div(
+      class = class,
       tags$input(
         id = id,
         class = "form-check-input",
         type = "checkbox",
+        role = "switch",
         checked = if (value) NA,
       ),
       tags$label(
@@ -36,12 +47,8 @@ input_checkbox <- function(id, label, class = "form-check", value = FALSE, width
         # https://github.com/rstudio/shiny/blob/c21ba0b/srcts/src/bindings/input/checkbox.ts#L42-L43
         tags$span(label),
         class = "form-check-label",
-        `for_` = id
-      ),
-      class = class,
-    ),
-    class = "form-group shiny-input-container",
-    class = if (inline) "shiny-input-container-inline",
-    style = css(width = width),
+        `for` = id
+      )
+    )
   )
 }
