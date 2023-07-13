5d0d951c4d148e19f9b57a28da0ac5ef942e2a7e
Carson <cpsievert1@gmail.com>
2023-07-13 17:40:24 -0500

    Embrace update/toggle as a prefix; doc improvements

--- R/tooltip.R
+++ R/tooltip.R
@@ -1,25 +1,25 @@
 #' Add a tooltip to a UI element
 #'
-#' Tooltips are useful for showing additional information when focusing (or
-#' hovering over) a UI element.
+#' Display additional information when focusing (or hovering over) a UI element.
 #'
 #' @param trigger A UI element (i.e., [htmltools tag][htmltools::tags]) to serve
 #'   as the tooltips trigger. It's good practice for this element to be a
 #'   keyboard-focusable and interactive element (e.g., `actionButton()`,
 #'   `actionLink()`, etc) so that the tooltip is accessible to keyboard and
 #'   assistive technology users.
-#' @param ... UI elements for the tooltip. Character strings are automatically
-#'   [htmlEscape()]d unless marked as [HTML()].
-#' @param id If provided, you can use `input$id` in your server logic to
-#'   determine whether the tooltip is currently shown/hidden.
+#' @param ... UI elements for the tooltip. Character strings are [automatically
+#'   escaped][htmlEscape()] unless marked as [HTML()].
+#' @param id A character string. Required to re-actively respond to the
+#'   visibility of the tooltip (via the `input[[id]]` value) and/or update the
+#'   visibility/contents of the tooltip.
 #' @param placement The placement of the tooltip relative to its trigger.
 #' @param options A list of additional [Bootstrap
 #'   options](https://getbootstrap.com/docs/5.3/components/tooltips/#options).
 #'
-#' @details If `trigger` yields multiple HTML elements (i.e., it's a `tagList()`
-#'   or complex `{htmlwidgets}` object), the last HTML element is used as the
-#'   tooltip's target. If the `trigger` should contain all of those elements,
-#'   consider wrapping the object in a [span()] or [div()].
+#' @details If `trigger` yields multiple HTML elements (e.g., a `tagList()` or
+#'   complex `{htmlwidgets}` object), the last HTML element is used as the
+#'   trigger. If the `trigger` should contain all of those elements, wrap the
+#'   object in a [div()] or [span()].
 #'
 #' @describeIn tooltip Add a tooltip to a UI element
 #' @references <https://getbootstrap.com/docs/5.3/components/tooltips/>
@@ -80,11 +80,13 @@ tooltip <- function(
 #' @param id a character string that matches an existing tooltip id.
 #' @param show Whether to show (`TRUE`) or hide (`FALSE`) the tooltip. The
 #'   default (`NULL`) will show if currently hidden and hide if currently shown.
+#'   Note that a tooltip will not be shown if the trigger is not visible (e.g.,
+#'   it's hidden behind a tab).
 #' @param session A Shiny session object (the default should almost always be
 #'   used).
 #'
 #' @export
-tooltip_toggle <- function(id, show = NULL, session = get_current_session()) {
+toggle_tooltip <- function(id, show = NULL, session = get_current_session()) {
   show <- normalize_show_value(show)
 
   msg <- list(method = "toggle", value = show)
@@ -98,7 +100,7 @@ tooltip_toggle <- function(id, show = NULL, session = get_current_session()) {
 
 #' @describeIn tooltip Update the contents of a tooltip.
 #' @export
-tooltip_update <- function(id, ..., session = get_current_session()) {
+update_tooltip <- function(id, ..., session = get_current_session()) {
 
   title <- tagList(...)
 
