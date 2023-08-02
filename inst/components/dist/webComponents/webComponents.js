3ea6aa5f9136c088d65c3b95ee80e006c1377050
cpsievert <cpsievert@users.noreply.github.com>
2023-08-02 23:01:57 +0000

    `yarn build` (GitHub Actions)

--- inst/components/dist/webComponents/webComponents.js
+++ inst/components/dist/webComponents/webComponents.js
@@ -51,12 +51,31 @@
     });
   };
 
+  // node_modules/@lit/reactive-element/decorators/property.js
+  var i = (i4, e6) => "method" === e6.kind && e6.descriptor && !("value" in e6.descriptor) ? __spreadProps(__spreadValues({}, e6), { finisher(n7) {
+    n7.createProperty(e6.key, i4);
+  } }) : { kind: "field", key: Symbol(), placement: "own", descriptor: {}, originalKey: e6.key, initializer() {
+    "function" == typeof e6.initializer && (this[e6.key] = e6.initializer.call(this));
+  }, finisher(n7) {
+    n7.createProperty(e6.key, i4);
+  } };
+  var e = (i4, e6, n7) => {
+    e6.constructor.createProperty(n7, i4);
+  };
+  function n(n7) {
+    return (t3, o6) => void 0 !== o6 ? e(n7, t3, o6) : i(n7, t3);
+  }
+
+  // node_modules/@lit/reactive-element/decorators/query-assigned-elements.js
+  var n2;
+  var e2 = null != (null === (n2 = window.HTMLSlotElement) || void 0 === n2 ? void 0 : n2.prototype.assignedElements) ? (o6, n7) => o6.assignedElements(n7) : (o6, n7) => o6.assignedNodes(n7).filter((o7) => o7.nodeType === Node.ELEMENT_NODE);
+
   // node_modules/@lit/reactive-element/css-tag.js
   var t = window;
-  var e = t.ShadowRoot && (void 0 === t.ShadyCSS || t.ShadyCSS.nativeShadow) && "adoptedStyleSheets" in Document.prototype && "replace" in CSSStyleSheet.prototype;
+  var e3 = t.ShadowRoot && (void 0 === t.ShadyCSS || t.ShadyCSS.nativeShadow) && "adoptedStyleSheets" in Document.prototype && "replace" in CSSStyleSheet.prototype;
   var s = Symbol();
-  var n = /* @__PURE__ */ new WeakMap();
-  var o = class {
+  var n3 = /* @__PURE__ */ new WeakMap();
+  var o2 = class {
     constructor(t3, e6, n7) {
       if (this._$cssResult$ = true, n7 !== s)
         throw Error("CSSResult is not constructable. Use `unsafeCSS` or `css` instead.");
@@ -65,9 +84,9 @@
     get styleSheet() {
       let t3 = this.o;
       const s5 = this.t;
-      if (e && void 0 === t3) {
+      if (e3 && void 0 === t3) {
         const e6 = void 0 !== s5 && 1 === s5.length;
-        e6 && (t3 = n.get(s5)), void 0 === t3 && ((this.o = t3 = new CSSStyleSheet()).replaceSync(this.cssText), e6 && n.set(s5, t3));
+        e6 && (t3 = n3.get(s5)), void 0 === t3 && ((this.o = t3 = new CSSStyleSheet()).replaceSync(this.cssText), e6 && n3.set(s5, t3));
       }
       return t3;
     }
@@ -75,14 +94,24 @@
       return this.cssText;
     }
   };
-  var r = (t3) => new o("string" == typeof t3 ? t3 : t3 + "", void 0, s);
+  var r = (t3) => new o2("string" == typeof t3 ? t3 : t3 + "", void 0, s);
+  var i2 = (t3, ...e6) => {
+    const n7 = 1 === t3.length ? t3[0] : e6.reduce((e7, s5, n8) => e7 + ((t4) => {
+      if (true === t4._$cssResult$)
+        return t4.cssText;
+      if ("number" == typeof t4)
+        return t4;
+      throw Error("Value passed to 'css' function must be a 'css' function result: " + t4 + ". Use 'unsafeCSS' to pass non-literal values, but take care to ensure page security.");
+    })(s5) + t3[n8 + 1], t3[0]);
+    return new o2(n7, t3, s);
+  };
   var S = (s5, n7) => {
-    e ? s5.adoptedStyleSheets = n7.map((t3) => t3 instanceof CSSStyleSheet ? t3 : t3.styleSheet) : n7.forEach((e6) => {
+    e3 ? s5.adoptedStyleSheets = n7.map((t3) => t3 instanceof CSSStyleSheet ? t3 : t3.styleSheet) : n7.forEach((e6) => {
       const n8 = document.createElement("style"), o6 = t.litNonce;
       void 0 !== o6 && n8.setAttribute("nonce", o6), n8.textContent = e6.cssText, s5.appendChild(n8);
     });
   };
-  var c = e ? (t3) => t3 : (t3) => t3 instanceof CSSStyleSheet ? ((t4) => {
+  var c = e3 ? (t3) => t3 : (t3) => t3 instanceof CSSStyleSheet ? ((t4) => {
     let e6 = "";
     for (const s5 of t4.cssRules)
       e6 += s5.cssText;
@@ -91,11 +120,11 @@
 
   // node_modules/@lit/reactive-element/reactive-element.js
   var s2;
-  var e2 = window;
-  var r2 = e2.trustedTypes;
+  var e4 = window;
+  var r2 = e4.trustedTypes;
   var h = r2 ? r2.emptyScript : "";
-  var o2 = e2.reactiveElementPolyfillSupport;
-  var n2 = { toAttribute(t3, i4) {
+  var o3 = e4.reactiveElementPolyfillSupport;
+  var n4 = { toAttribute(t3, i4) {
     switch (i4) {
       case Boolean:
         t3 = t3 ? h : null;
@@ -125,7 +154,7 @@
     return s5;
   } };
   var a = (t3, i4) => i4 !== t3 && (i4 == i4 || t3 == t3);
-  var l = { attribute: true, type: String, converter: n2, reflect: false, hasChanged: a };
+  var l2 = { attribute: true, type: String, converter: n4, reflect: false, hasChanged: a };
   var d = "finalized";
   var u = class extends HTMLElement {
     constructor() {
@@ -143,7 +172,7 @@
         void 0 !== e6 && (this._$Ev.set(e6, s5), t3.push(e6));
       }), t3;
     }
-    static createProperty(t3, i4 = l) {
+    static createProperty(t3, i4 = l2) {
       if (i4.state && (i4.attribute = false), this.finalize(), this.elementProperties.set(t3, i4), !i4.noAccessor && !this.prototype.hasOwnProperty(t3)) {
         const s5 = "symbol" == typeof t3 ? Symbol() : "__" + t3, e6 = this.getPropertyDescriptor(t3, s5, i4);
         void 0 !== e6 && Object.defineProperty(this.prototype, t3, e6);
@@ -158,7 +187,7 @@
       }, configurable: true, enumerable: true };
     }
     static getPropertyOptions(t3) {
-      return this.elementProperties.get(t3) || l;
+      return this.elementProperties.get(t3) || l2;
     }
     static finalize() {
       if (this.hasOwnProperty(d))
@@ -227,11 +256,11 @@
     attributeChangedCallback(t3, i4, s5) {
       this._$AK(t3, s5);
     }
-    _$EO(t3, i4, s5 = l) {
+    _$EO(t3, i4, s5 = l2) {
       var e6;
       const r4 = this.constructor._$Ep(t3, s5);
       if (void 0 !== r4 && true === s5.reflect) {
-        const h3 = (void 0 !== (null === (e6 = s5.converter) || void 0 === e6 ? void 0 : e6.toAttribute) ? s5.converter : n2).toAttribute(i4, s5.type);
+        const h3 = (void 0 !== (null === (e6 = s5.converter) || void 0 === e6 ? void 0 : e6.toAttribute) ? s5.converter : n4).toAttribute(i4, s5.type);
         this._$El = t3, null == h3 ? this.removeAttribute(r4) : this.setAttribute(r4, h3), this._$El = null;
       }
     }
@@ -239,7 +268,7 @@
       var s5;
       const e6 = this.constructor, r4 = e6._$Ev.get(t3);
       if (void 0 !== r4 && this._$El !== r4) {
-        const t4 = e6.getPropertyOptions(r4), h3 = "function" == typeof t4.converter ? { fromAttribute: t4.converter } : void 0 !== (null === (s5 = t4.converter) || void 0 === s5 ? void 0 : s5.fromAttribute) ? t4.converter : n2;
+        const t4 = e6.getPropertyOptions(r4), h3 = "function" == typeof t4.converter ? { fromAttribute: t4.converter } : void 0 !== (null === (s5 = t4.converter) || void 0 === s5 ? void 0 : s5.fromAttribute) ? t4.converter : n4;
         this._$El = r4, this[r4] = h3.fromAttribute(i4, t4.type), this._$El = null;
       }
     }
@@ -308,17 +337,17 @@
     firstUpdated(t3) {
     }
   };
-  u[d] = true, u.elementProperties = /* @__PURE__ */ new Map(), u.elementStyles = [], u.shadowRootOptions = { mode: "open" }, null == o2 || o2({ ReactiveElement: u }), (null !== (s2 = e2.reactiveElementVersions) && void 0 !== s2 ? s2 : e2.reactiveElementVersions = []).push("1.6.2");
+  u[d] = true, u.elementProperties = /* @__PURE__ */ new Map(), u.elementStyles = [], u.shadowRootOptions = { mode: "open" }, null == o3 || o3({ ReactiveElement: u }), (null !== (s2 = e4.reactiveElementVersions) && void 0 !== s2 ? s2 : e4.reactiveElementVersions = []).push("1.6.2");
 
   // node_modules/lit-html/lit-html.js
   var t2;
-  var i2 = window;
-  var s3 = i2.trustedTypes;
-  var e3 = s3 ? s3.createPolicy("lit-html", { createHTML: (t3) => t3 }) : void 0;
-  var o3 = "$lit$";
-  var n3 = `lit$${(Math.random() + "").slice(9)}$`;
-  var l2 = "?" + n3;
-  var h2 = `<${l2}>`;
+  var i3 = window;
+  var s3 = i3.trustedTypes;
+  var e5 = s3 ? s3.createPolicy("lit-html", { createHTML: (t3) => t3 }) : void 0;
+  var o4 = "$lit$";
+  var n5 = `lit$${(Math.random() + "").slice(9)}$`;
+  var l3 = "?" + n5;
+  var h2 = `<${l3}>`;
   var r3 = document;
   var u2 = () => r3.createComment("");
   var d2 = (t3) => null === t3 || "object" != typeof t3 && "function" != typeof t3;
@@ -343,7 +372,7 @@
   function P(t3, i4) {
     if (!Array.isArray(t3) || !t3.hasOwnProperty("raw"))
       throw Error("invalid template strings array");
-    return void 0 !== e3 ? e3.createHTML(i4) : i4;
+    return void 0 !== e5 ? e5.createHTML(i4) : i4;
   }
   var V = (t3, i4) => {
     const s5 = t3.length - 1, e6 = [];
@@ -354,7 +383,7 @@
       for (; a3 < s6.length && (u3.lastIndex = a3, c3 = u3.exec(s6), null !== c3); )
         a3 = u3.lastIndex, u3 === f ? "!--" === c3[1] ? u3 = _ : void 0 !== c3[1] ? u3 = m : void 0 !== c3[2] ? (y.test(c3[2]) && (l5 = RegExp("</" + c3[2], "g")), u3 = p) : void 0 !== c3[3] && (u3 = p) : u3 === p ? ">" === c3[0] ? (u3 = null != l5 ? l5 : f, v2 = -1) : void 0 === c3[1] ? v2 = -2 : (v2 = u3.lastIndex - c3[2].length, d3 = c3[1], u3 = void 0 === c3[3] ? p : '"' === c3[3] ? $2 : g) : u3 === $2 || u3 === g ? u3 = p : u3 === _ || u3 === m ? u3 = f : (u3 = p, l5 = void 0);
       const w2 = u3 === p && t3[i5 + 1].startsWith("/>") ? " " : "";
-      r4 += u3 === f ? s6 + h2 : v2 >= 0 ? (e6.push(d3), s6.slice(0, v2) + o3 + s6.slice(v2) + n3 + w2) : s6 + n3 + (-2 === v2 ? (e6.push(void 0), i5) : w2);
+      r4 += u3 === f ? s6 + h2 : v2 >= 0 ? (e6.push(d3), s6.slice(0, v2) + o4 + s6.slice(v2) + n5 + w2) : s6 + n5 + (-2 === v2 ? (e6.push(void 0), i5) : w2);
     }
     return [P(t3, r4 + (t3[s5] || "<?>") + (2 === i4 ? "</svg>" : "")), e6];
   };
@@ -373,10 +402,10 @@
           if (h3.hasAttributes()) {
             const t4 = [];
             for (const i5 of h3.getAttributeNames())
-              if (i5.endsWith(o3) || i5.startsWith(n3)) {
+              if (i5.endsWith(o4) || i5.startsWith(n5)) {
                 const s5 = f2[d3++];
                 if (t4.push(i5), void 0 !== s5) {
-                  const t5 = h3.getAttribute(s5.toLowerCase() + o3).split(n3), i6 = /([.?@])?(.*)/.exec(s5);
+                  const t5 = h3.getAttribute(s5.toLowerCase() + o4).split(n5), i6 = /([.?@])?(.*)/.exec(s5);
                   v2.push({ type: 1, index: r4, name: i6[2], strings: t5, ctor: "." === i6[1] ? H : "?" === i6[1] ? L : "@" === i6[1] ? z : k });
                 } else
                   v2.push({ type: 6, index: r4 });
@@ -385,7 +414,7 @@
               h3.removeAttribute(i5);
           }
           if (y.test(h3.tagName)) {
-            const t4 = h3.textContent.split(n3), i5 = t4.length - 1;
+            const t4 = h3.textContent.split(n5), i5 = t4.length - 1;
             if (i5 > 0) {
               h3.textContent = s3 ? s3.emptyScript : "";
               for (let s5 = 0; s5 < i5; s5++)
@@ -394,12 +423,12 @@
             }
           }
         } else if (8 === h3.nodeType)
-          if (h3.data === l2)
+          if (h3.data === l3)
             v2.push({ type: 2, index: r4 });
           else {
             let t4 = -1;
-            for (; -1 !== (t4 = h3.data.indexOf(n3, t4 + 1)); )
-              v2.push({ type: 7, index: r4 }), t4 += n3.length - 1;
+            for (; -1 !== (t4 = h3.data.indexOf(n5, t4 + 1)); )
+              v2.push({ type: 7, index: r4 }), t4 += n5.length - 1;
           }
         r4++;
       }
@@ -584,8 +613,8 @@
       S2(this, t3);
     }
   };
-  var B = i2.litHtmlPolyfillSupport;
-  null == B || B(N, R), (null !== (t2 = i2.litHtmlVersions) && void 0 !== t2 ? t2 : i2.litHtmlVersions = []).push("2.7.5");
+  var B = i3.litHtmlPolyfillSupport;
+  null == B || B(N, R), (null !== (t2 = i3.litHtmlVersions) && void 0 !== t2 ? t2 : i3.litHtmlVersions = []).push("2.7.5");
   var D = (t3, i4, s5) => {
     var e6, o6;
     const n7 = null !== (e6 = null == s5 ? void 0 : s5.renderBefore) && void 0 !== e6 ? e6 : i4;
@@ -598,8 +627,8 @@
   };
 
   // node_modules/lit-element/lit-element.js
-  var l3;
-  var o4;
+  var l4;
+  var o5;
   var s4 = class extends u {
     constructor() {
       super(...arguments), this.renderOptions = { host: this }, this._$Do = void 0;
@@ -625,46 +654,19 @@
       return T;
     }
   };
-  s4.finalized = true, s4._$litElement$ = true, null === (l3 = globalThis.litElementHydrateSupport) || void 0 === l3 || l3.call(globalThis, { LitElement: s4 });
-  var n4 = globalThis.litElementPolyfillSupport;
-  null == n4 || n4({ LitElement: s4 });
-  (null !== (o4 = globalThis.litElementVersions) && void 0 !== o4 ? o4 : globalThis.litElementVersions = []).push("3.3.2");
+  s4.finalized = true, s4._$litElement$ = true, null === (l4 = globalThis.litElementHydrateSupport) || void 0 === l4 || l4.call(globalThis, { LitElement: s4 });
+  var n6 = globalThis.litElementPolyfillSupport;
+  null == n6 || n6({ LitElement: s4 });
+  (null !== (o5 = globalThis.litElementVersions) && void 0 !== o5 ? o5 : globalThis.litElementVersions = []).push("3.3.2");
 
-  // node_modules/@lit/reactive-element/decorators/property.js
-  var i3 = (i4, e6) => "method" === e6.kind && e6.descriptor && !("value" in e6.descriptor) ? __spreadProps(__spreadValues({}, e6), { finisher(n7) {
-    n7.createProperty(e6.key, i4);
-  } }) : { kind: "field", key: Symbol(), placement: "own", descriptor: {}, originalKey: e6.key, initializer() {
-    "function" == typeof e6.initializer && (this[e6.key] = e6.initializer.call(this));
-  }, finisher(n7) {
-    n7.createProperty(e6.key, i4);
-  } };
-  var e4 = (i4, e6, n7) => {
-    e6.constructor.createProperty(n7, i4);
-  };
-  function n5(n7) {
-    return (t3, o6) => void 0 !== o6 ? e4(n7, t3, o6) : i3(n7, t3);
-  }
-
-  // node_modules/@lit/reactive-element/decorators/query-assigned-elements.js
-  var n6;
-  var e5 = null != (null === (n6 = window.HTMLSlotElement) || void 0 === n6 ? void 0 : n6.prototype.assignedElements) ? (o6, n7) => o6.assignedElements(n7) : (o6, n7) => o6.assignedNodes(n7).filter((o7) => o7.nodeType === Node.ELEMENT_NODE);
-
-  // srcts/src/components/webcomponents/_lightElement.ts
-  var LightElement = class extends s4 {
-    constructor() {
-      super(...arguments);
-      this.elementChildren = [];
-    }
+  // srcts/src/components/webcomponents/bslibElement.ts
+  var BslibElement = class extends s4 {
     connectedCallback() {
-      this.elementChildren = Array.from(this.childNodes);
       this.maybeCarryFill();
       super.connectedCallback();
     }
-    get slotElements() {
-      return this.elementChildren;
-    }
-    createRenderRoot() {
-      return this;
+    render() {
+      return x`<slot></slot>`;
     }
     maybeCarryFill() {
       if (this.isFillCarrier) {
@@ -688,7 +690,13 @@
       return inContainer && hasFillItem;
     }
   };
-  LightElement.isShinyInput = false;
+  BslibElement.tagName = "bslib-element";
+  BslibElement.isShinyInput = false;
+  BslibElement.styles = i2`
+    :host {
+      display: contents;
+    }
+  `;
 
   // srcts/src/components/_utilsTooltip.ts
   function getOrCreateTriggerEl(el) {
@@ -847,7 +855,7 @@
   // srcts/src/components/tooltip.ts
   var bsTooltip = window.bootstrap ? window.bootstrap.Tooltip : class {
   };
-  var _BslibTooltip = class extends LightElement {
+  var _BslibTooltip = class extends BslibElement {
     ///////////////////////////////////////////////////////////////
     // Methods
     ///////////////////////////////////////////////////////////////
@@ -866,7 +874,6 @@
       this._onShown = this._onShown.bind(this);
       this._onInsert = this._onInsert.bind(this);
       this._onHidden = this._onHidden.bind(this);
-      this.style.display = "contents";
     }
     get options() {
       const opts = JSON.parse(this.bsOptions);
@@ -909,9 +916,6 @@
       this.visibilityObserver.disconnect();
       this.bsTooltip.dispose();
       super.disconnectedCallback();
-    }
-    render() {
-      return A;
     }
     getValue() {
       return this.visible;
@@ -1020,16 +1024,16 @@
   // Shiny-specific stuff
   BslibTooltip.isShinyInput = true;
   __decorateClass([
-    n5({ type: String })
+    n({ type: String })
   ], BslibTooltip.prototype, "placement", 2);
   __decorateClass([
-    n5({ type: String })
+    n({ type: String })
   ], BslibTooltip.prototype, "bsOptions", 2);
 
   // srcts/src/components/popover.ts
   var bsPopover = window.bootstrap ? window.bootstrap.Popover : class {
   };
-  var _BslibPopover = class extends LightElement {
+  var _BslibPopover = class extends BslibElement {
     ///////////////////////////////////////////////////////////////
     // Methods
     ///////////////////////////////////////////////////////////////
@@ -1053,7 +1057,6 @@
       this._handleTabKey = this._handleTabKey.bind(this);
       this._handleEscapeKey = this._handleEscapeKey.bind(this);
       this._closeButton = this._closeButton.bind(this);
-      this.style.display = "contents";
     }
     get options() {
       const opts = JSON.parse(this.bsOptions);
@@ -1141,9 +1144,6 @@
       this.visibilityObserver.disconnect();
       this.bsPopover.dispose();
       super.disconnectedCallback();
-    }
-    render() {
-      return A;
     }
     getValue() {
       return this.visible;
@@ -1353,10 +1353,10 @@
   ///////////////////////////////////////////////////////////////
   BslibPopover.isShinyInput = true;
   __decorateClass([
-    n5({ type: String })
+    n({ type: String })
   ], BslibPopover.prototype, "placement", 2);
   __decorateClass([
-    n5({ type: String })
+    n({ type: String })
   ], BslibPopover.prototype, "bsOptions", 2);
   function hasHeader(header) {
     return !!header && header.childNodes.length > 0;
@@ -1406,48 +1406,13 @@
   }
 
   // srcts/src/components/webComponents.ts
-  [BslibTooltip, BslibPopover].forEach((cls) => {
+  [BslibTooltip, BslibPopover, BslibElement].forEach((cls) => {
     customElements.define(cls.tagName, cls);
     if (cls.isShinyInput)
       makeInputBinding(cls.tagName);
   });
 })();
 /*! Bundled license information:
-
-@lit/reactive-element/css-tag.js:
-  (**
-   * @license
-   * Copyright 2019 Google LLC
-   * SPDX-License-Identifier: BSD-3-Clause
-   *)
-
-@lit/reactive-element/reactive-element.js:
-  (**
-   * @license
-   * Copyright 2017 Google LLC
-   * SPDX-License-Identifier: BSD-3-Clause
-   *)
-
-lit-html/lit-html.js:
-  (**
-   * @license
-   * Copyright 2017 Google LLC
-   * SPDX-License-Identifier: BSD-3-Clause
-   *)
-
-lit-element/lit-element.js:
-  (**
-   * @license
-   * Copyright 2017 Google LLC
-   * SPDX-License-Identifier: BSD-3-Clause
-   *)
-
-lit-html/is-server.js:
-  (**
-   * @license
-   * Copyright 2022 Google LLC
-   * SPDX-License-Identifier: BSD-3-Clause
-   *)
 
 @lit/reactive-element/decorators/custom-element.js:
   (**
@@ -1516,6 +1481,41 @@ lit-html/is-server.js:
   (**
    * @license
    * Copyright 2017 Google LLC
+   * SPDX-License-Identifier: BSD-3-Clause
+   *)
+
+@lit/reactive-element/css-tag.js:
+  (**
+   * @license
+   * Copyright 2019 Google LLC
+   * SPDX-License-Identifier: BSD-3-Clause
+   *)
+
+@lit/reactive-element/reactive-element.js:
+  (**
+   * @license
+   * Copyright 2017 Google LLC
+   * SPDX-License-Identifier: BSD-3-Clause
+   *)
+
+lit-html/lit-html.js:
+  (**
+   * @license
+   * Copyright 2017 Google LLC
+   * SPDX-License-Identifier: BSD-3-Clause
+   *)
+
+lit-element/lit-element.js:
+  (**
+   * @license
+   * Copyright 2017 Google LLC
+   * SPDX-License-Identifier: BSD-3-Clause
+   *)
+
+lit-html/is-server.js:
+  (**
+   * @license
+   * Copyright 2022 Google LLC
    * SPDX-License-Identifier: BSD-3-Clause
    *)
 */
