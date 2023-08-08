a11f16bfd7e99da40fd65f4807e5eea0238bcacc
Carson <cpsievert1@gmail.com>
2023-08-08 14:40:42 -0500

    Move imports to R/bslib-package.R

--- R/bslib-package.R
+++ R/bslib-package.R
@@ -1,6 +1,17 @@
+utils::globalVariables("!!")
+
 #' @keywords internal
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import htmltools
+#' @import sass
+#' @importFrom utils modifyList packageVersion download.file URLencode getFromNamespace head
+#' @importFrom stats setNames na.omit
+#' @importFrom grDevices col2rgb
+#' @importFrom tools file_path_sans_ext
+#' @importFrom jquerylib jquery_core
+#' @importFrom jsonlite fromJSON
+#' @importFrom rlang := list2 abort
 ## usethis namespace: end
 NULL
