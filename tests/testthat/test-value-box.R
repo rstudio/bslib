928ef9b04afec2479c89221975b7d03b94c340ee
Garrick Aden-Buie <garrick@adenbuie.com>
2023-08-24 09:48:20 -0400

    tests(showcase): Add tests for `resolve_showcase_layout()`

--- tests/testthat/test-value-box.R
+++ tests/testthat/test-value-box.R
@@ -17,3 +17,37 @@ test_that("value_box(theme_color=) is deprecated", {
     )
   )
 })
+
+test_that("resolve_showcase_layout()", {
+  expect_equal(
+    resolve_showcase_layout("left center"),
+    showcase_left_center()
+  )
+
+  expect_equal(
+    resolve_showcase_layout("top right"),
+    showcase_top_right()
+  )
+
+  expect_equal(
+    resolve_showcase_layout("bottom"),
+    showcase_bottom()
+  )
+
+  expect_equal(
+    resolve_showcase_layout(showcase_left_center(width = 0.5)),
+    showcase_left_center(width = 0.5)
+  )
+
+  expect_error(
+    resolve_showcase_layout("left"),
+  )
+})
+
+test_that("resolve_showcase_layout() can resolve all layouts in value_box()", {
+  layouts <- eval(rlang::fn_fmls(value_box)$showcase_layout)
+
+  for (layout in layouts) {
+    expect_silent(resolve_showcase_layout(layout))
+  }
+})
