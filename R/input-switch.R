1c6bddf91adb10b5a00542a70d96ef66756e713f
Garrick Aden-Buie <garrick@adenbuie.com>
2023-08-29 16:48:04 -0400

    feat(toggle_switch): Validate `id` input

--- R/input-switch.R
+++ R/input-switch.R
@@ -64,6 +64,10 @@ toggle_switch <- function(id, value = NULL, session = get_current_session()) {
     abort("`value` must be a `NULL` or a single logical value.")
   }
 
+  if (!rlang::is_string(id, n = 1)) {
+    abort("`id` must be a single string containing the `id` of a switch input.")
+  }
+
   msg <- dropNulls(list(id = session$ns(id), value = value))
 
   callback <- function() {
