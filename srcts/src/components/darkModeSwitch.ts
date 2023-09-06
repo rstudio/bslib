34552c50376533bb8056fa4dcecb78adf869b4e5
gadenbuie <gadenbuie@users.noreply.github.com>
2023-09-06 15:38:31 +0000

    `yarn build` (GitHub Actions)

--- srcts/src/components/darkModeSwitch.ts
+++ srcts/src/components/darkModeSwitch.ts
@@ -13,8 +13,8 @@ type ToggleMessage = {
   value: DarkModeSwitch["themeValue"] | "toggle";
 };
 
-// This class extends BslibElement only as a convenience for registering the custom element 
-// and Shiny input binding. All the logic here (besides the `isShinyInput`/`tagName` fields) is 
+// This class extends BslibElement only as a convenience for registering the custom element
+// and Shiny input binding. All the logic here (besides the `isShinyInput`/`tagName` fields) is
 // designed to work without Bootstrap/bslib
 export class DarkModeSwitch extends BslibElement {
   static isShinyInput = true;
