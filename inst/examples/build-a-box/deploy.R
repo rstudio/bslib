c7c60bfcc1c4e80eb4829a52c42aab553a3d3992
Garrick Aden-Buie <garrick@adenbuie.com>
2023-09-08 10:13:43 -0400

    ci: shinyapps.io doesn't support `envVars`

--- inst/examples/build-a-box/deploy.R
+++ inst/examples/build-a-box/deploy.R
@@ -2,6 +2,5 @@ rsconnect::deployApp(
   rprojroot::find_package_root_file("inst/examples/build-a-box"),
   appName = "build-a-box",
   account = "testing-apps",
-  forceUpdate = TRUE,
-  envVars = c(ENABLE_THEMER = "true")
+  forceUpdate = TRUE
 )
