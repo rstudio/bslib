27dd66b5c2d36e9a7724f8de2fd86631f16d081e
Carson <cpsievert1@gmail.com>
2023-09-18 11:06:56 -0500

    Deploy all example apps to bslib user

--- inst/examples/flights/deploy.R
+++ inst/examples/flights/deploy.R
@@ -1,6 +1,6 @@
 rsconnect::deployApp(
   rprojroot::find_package_root_file("inst/examples/flights"),
   appName = "flights",
-  account = "testing-apps",
+  account = "bslib",
   forceUpdate = TRUE
 )
--- inst/themer-demo/deploy.R
+++ inst/themer-demo/deploy.R
@@ -1,6 +1,6 @@
 rsconnect::deployApp(
   rprojroot::find_package_root_file("inst/themer-demo/deploy"),
   appName = "themer-demo",
-  account = "testing-apps",
+  account = "bslib",
   forceUpdate = TRUE
 )
