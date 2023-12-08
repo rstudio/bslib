e4627e39e1d104a3fddededabe5bca9f1a22ba2f
Garrick Aden-Buie <garrick@adenbuie.com>
2023-12-08 17:02:13 -0500

    refactor(layout_columns): Now a Custom Element (#931)
    * feat(BslibLayoutColumns): Add custom element component
    
    * chore: move comments from R code
    
    * chore(layout_columns): Remove all the R code that is now in JavaScript
    
    * chore: small code relocating
    
    * refactor: simplify any/all isNA checks
    
    * feat(bslib-grid): Use CSS property to set fallback grid item span
    
    * chore: rlang::is_na() is too aggressive for list(NA)
    
    * chore: use `web_component()` helper
    
    * chore(bslib-grid): Use lower specificty selector for fallback item span
    
    * docs: Add news item
    
    * feat: support `[hidden-until-init]` attribute
    
    Hides the element until the colwidth spec is applied. Not used yet.
    
    * feat(layout_columns): Set row heights on all sizes if only one value is given
    
    * docs: Add news item for `row_heights`
    
    * refactor: row_heights at xs are equivalent to setting at all breakpoints
    
    * chore: Add note about `hidden-until-init` attribute
    
    * refactor: rename `col_spec` and `as_col_spec()`
    
    * feat(maybe_fr_unit): Round fractional fr units to integers
    
    * chore: add a comment about the xs breakpoint
    
    * feat: support `xs` as a breakpoint for col_widths in javascript
    
    * feat(breakpoints): Add `xs` param and document its use as base case
    
    * refactor: improve handling of the xs breakpoints case
    
    * docs: Add news about col_widths default span setting
    
    * fix typo in layout.R
    
    * docs: reflow news bullet
    
    * refactor(bslib-layout-columns): Store column widths as attributes, not JSON
    
    * fix: Exclude offest columns from oversized `col_widths` warning
    
    * fix: layout_column_wrap() and layout_columns() share .bslib-grid
    
    * chore: build component.css

--- srcts/src/components/webcomponents/index.ts
+++ srcts/src/components/webcomponents/index.ts
@@ -3,13 +3,16 @@ import { BslibPopover } from "./popover";
 import { BslibInputDarkMode } from "./inputDarkMode";
 import { makeInputBinding } from "./_makeInputBinding";
 import { shinyAddCustomMessageHandlers } from "../_shinyAddCustomMessageHandlers";
+import { BslibLayoutColumns } from "./layoutColumns";
 
-[BslibTooltip, BslibPopover, BslibInputDarkMode].forEach((cls) => {
-  customElements.define(cls.tagName, cls);
-  if (window.Shiny) {
-    if (cls.isShinyInput) makeInputBinding(cls.tagName);
-    if ("shinyCustomMessageHandlers" in cls) {
-      shinyAddCustomMessageHandlers(cls["shinyCustomMessageHandlers"]);
+[BslibTooltip, BslibPopover, BslibInputDarkMode, BslibLayoutColumns].forEach(
+  (cls) => {
+    customElements.define(cls.tagName, cls);
+    if (window.Shiny) {
+      if (cls.isShinyInput) makeInputBinding(cls.tagName);
+      if ("shinyCustomMessageHandlers" in cls) {
+        shinyAddCustomMessageHandlers(cls["shinyCustomMessageHandlers"]);
+      }
     }
   }
-});
+);
