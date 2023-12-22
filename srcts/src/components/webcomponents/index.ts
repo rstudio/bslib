33a8b636a8f9dd172f347959a26ac720a0b1f2a4
Joe Cheng <joe@posit.co>
2023-12-21 16:34:51 -0800

    Refactor task button
    Extracted out BslibTaskButtonContents and changed it into a
    pair of more generic content-switcher custom elements.

--- srcts/src/components/webcomponents/index.ts
+++ srcts/src/components/webcomponents/index.ts
@@ -1,17 +1,18 @@
 import { BslibTooltip } from "./tooltip";
 import { BslibPopover } from "./popover";
-import { BslibTaskButtonContents } from "./taskButton";
 import { BslibInputDarkMode } from "./inputDarkMode";
 import { makeInputBinding } from "./_makeInputBinding";
 import { shinyAddCustomMessageHandlers } from "../_shinyAddCustomMessageHandlers";
 import { BslibLayoutColumns } from "./layoutColumns";
+import { BslibSwitch, BslibSwitchInline } from "./switch";
 
 [
   BslibTooltip,
   BslibPopover,
   BslibInputDarkMode,
   BslibLayoutColumns,
-  BslibTaskButtonContents,
+  BslibSwitch,
+  BslibSwitchInline,
 ].forEach((cls) => {
   customElements.define(cls.tagName, cls);
   if (window.Shiny) {
