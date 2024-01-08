4ebcb7e088ea56889429101d197e0c5c48ad8d67
Joe Cheng <joe@posit.co>
2024-01-08 11:37:36 -0500

    Fix devtools::load_all("shiny")
    Without this, load_all and devtools::document() would fail when called
    on the shiny package, saying the required version of bslib was not
    found.

--- R/onLoad.R
+++ R/onLoad.R
@@ -4,10 +4,7 @@
   .dependency_cache <<- cachem::cache_mem(max_age = 1)
 
   # Call setup_input_handlers() when both bslib and shiny are loaded
-  if ("shiny" %in% loadedNamespaces()) {
-    # shiny is already loaded, go ahead and set up
-    setup_input_handlers()
-  } else {
+  if (!setup_input_handlers()) {
     # shiny is not yet loaded, if it ever gets loaded we'll register then
     setHook(packageEvent("shiny", "onLoad"), function(...) {
       setup_input_handlers()
@@ -17,6 +14,19 @@
 
 .dependency_cache <- NULL
 
+# Returns TRUE if input handlers were successfully loaded
 setup_input_handlers <- function() {
+  if (!"shiny" %in% loadedNamespaces()) {
+    return(FALSE)
+  }
+
+  if (!"registerInputHandler" %in% names(asNamespace("shiny"))) {
+    # In devtools::load_all(), the shiny namespace is loaded but empty at the
+    # time bslib loads. It's OK not to do the registration now, it'll just
+    # happen when shiny finishes loading.
+    return(FALSE)
+  }
+
   shiny::registerInputHandler("bslib.taskbutton", input_task_button_input_handler)
+  TRUE
 }
