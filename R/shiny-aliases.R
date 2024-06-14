c52d0d9278c85dbf95bd398a21cadbf3fca380a8
Garrick Aden-Buie <garrick@adenbuie.com>
2024-06-14 16:52:43 -0400

    docs: tweak aliases callout

--- R/shiny-aliases.R
+++ R/shiny-aliases.R
@@ -3,8 +3,8 @@ docs_callout_shiny_alias <- function(new, old) {
     paste0(
       "This function is an alias for `shiny::%s()` and is included to ",
       "maintain more consistent naming conventions in Shiny apps that use ",
-      "\\pkg{bslib}. The documentation may still refer to the original ",
-      "function names. You can replace `shiny::%s()` with `%s()`."
+      "\\pkg{bslib}. The documentation on this page may still refer to the ",
+      "original function names. You can replace `shiny::%s()` with `%s()`."
     ),
     old, old, new
   )
