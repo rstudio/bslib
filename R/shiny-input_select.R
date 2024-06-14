a6ea91ef8448ccea29a43ef28fc358fa931cee54
Garrick Aden-Buie <garrick@adenbuie.com>
2024-06-14 12:39:51 -0400

    feat: input_select

---
+++ R/shiny-input_select.R
@@ -0,0 +1,49 @@
+#' @inherit shiny::selectInput params return title description details sections references
+#'
+#' @inheritParams input_action_button
+#'
+#' @family Shiny input aliases
+#' @export
+input_select <- function(
+  id,
+  label,
+  choices,
+  selected = NULL,
+  multiple = FALSE,
+  selectize = FALSE, # Match Shiny for Python
+  width = NULL,
+  size = NULL
+) {
+  shiny::selectInput(
+    inputId = id,
+    label = label,
+    choices = choices,
+    selected = selected,
+    multiple = multiple,
+    selectize = selectize,
+    width = width,
+    size = size
+  )
+}
+
+#' @inherit shiny::updateSelectInput params return title description details sections references
+#' @inheritParams update_action_button
+#'
+#' @family Shiny update aliases
+#' @export
+update_select <- function(
+  id,
+  ...,
+  label = NULL,
+  choices = NULL,
+  selected = NULL,
+  session = get_current_session()
+) {
+  shiny::updateSelectInput(
+    session = session,
+    inputId = id,
+    label = label,
+    choices = choices,
+    selected = selected
+  )
+}
