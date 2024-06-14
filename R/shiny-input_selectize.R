7d08ead8c88e5c558c504bb7767d6242978d0845
Garrick Aden-Buie <garrick@adenbuie.com>
2024-06-14 12:41:25 -0400

    feat: input_selectize

---
+++ R/shiny-input_selectize.R
@@ -0,0 +1,45 @@
+#' @inherit shiny::selectizeInput params return title description details sections references
+#'
+#' @inheritParams input_action_button
+#'
+#' @family Shiny input aliases
+#' @export
+input_selectize <- function(
+  id,
+  ...,
+  options = NULL,
+  width = NULL
+) {
+  shiny::selectizeInput(
+    inputId = id,
+    ...,
+    options = options,
+    width = width
+  )
+}
+
+#' @inherit shiny::updateSelectizeInput params return title description details sections references
+#' @inheritParams update_action_button
+#'
+#' @family Shiny update aliases
+#' @export
+update_selectize <- function(
+  id,
+  ...,
+  label = NULL,
+  choices = NULL,
+  selected = NULL,
+  options = list(),
+  server = FALSE,
+  session = get_current_session()
+) {
+  shiny::updateSelectizeInput(
+    session = session,
+    inputId = id,
+    label = label,
+    choices = choices,
+    selected = selected,
+    options = options,
+    server = server
+  )
+}
