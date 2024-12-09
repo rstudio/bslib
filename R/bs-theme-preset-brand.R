debb70715181d505374f2069ee792ac1d7c9c038
Garrick Aden-Buie <garrick@adenbuie.com>
2024-12-09 14:40:36 -0500

    feat: Add brand fonts

--- R/bs-theme-preset-brand.R
+++ R/bs-theme-preset-brand.R
@@ -11,6 +11,7 @@ bs_preset_brand_bundle <- function(brand_preset = NULL) {
     brand_preset <- brand_resolve_preset()
   }
 
+  brand_fonts <- brand_sass_fonts(brand_preset$brand)
   brand_color_palette <- brand_sass_color_palette(brand_preset$brand)
   brand_color <- brand_sass_color(brand_preset$brand)
   brand_defaults <- brand_sass_defaults_bootstrap(brand_preset$brand)
@@ -35,10 +36,12 @@ bs_preset_brand_bundle <- function(brand_preset = NULL) {
         "//* ---- brand.color ---- *//",
         !!!brand_color$defaults,
         "//* ---- brand.typography ---- *//",
+        !!!brand_fonts$defaults,
         !!!brand_typography$defaults,
       ),
-      rules = list(
-        brand_color_palette$rules
+      rules = list2(
+        brand_color_palette$rules,
+        !!!brand_fonts$rules
       )
     )
   )
@@ -179,7 +182,188 @@ brand_sass_typography <- function(brand) {
 
   list(defaults = defaults)
 }
+
+brand_sass_fonts <- function(brand) {
+  fonts <- b_get(brand, "typography", "fonts")
+
+  if (is.null(fonts)) {
+    return(list(defaults = list(), rules = list()))
+  }
+
+  defaults <- list()
+  rules <- list()
+
+  for (font in fonts) {
+    var_name <- sprintf(
+      "brand-font-%s", 
+      gsub("[^a-z0-9-]+", "-", tolower(font$family))
+    )
+
+    font_obj <- 
+      switch(
+        font$source,
+        google = 
+          sass::font_google(
+            family = font$family,
+            wght = brand_remap_font_weight(font$weight) %||% seq(100, 900, by = 100),
+            ital = c("normal" = 0, "italic" = 1)[font$style],
+            display = font$display %||% "auto"
+          ),
+        bunny = 
+          brand_font_bunny(
+            family = font$family,
+            weight = font$weight,
+            style = font$style,
+            display = font$display
+          ),
+        file = brand_font_file(
+          family = font$family,
+          files = font$files,
+          brand_root = dirname(brand$path)
+        ),
+        system = NULL,
+        abort(sprintf("Unknown font source '%s'.", font$source))
+      )
+    
+    if (!is.null(font_obj)) {
+      defaults[[var_name]] <- font_obj
+      rules <- c(
+        rules,
+        sprintf(".brand-font-%s { font-family: $%s; }", var_name, var_name)
+      )
+    }
+  }
+
+  list(defaults = defaults, rules = rules)
 }
+
+brand_font_bunny <- function(family, weight = NULL, style = NULL, display = NULL) {
+  weight <- brand_remap_font_weight(weight) %||% seq(100, 900, 100)
+  
+  style <- style %||% c("normal", "italic")
+  style <- rlang::arg_match(
+    style,
+    values = c("normal", "italic"),
+    multiple = TRUE
+  )
+  
+  display <- display %||% "auto"
+  display <- rlang::arg_match(
+    display,
+    values = c("swap", "auto", "block", "fallback", "optional"),
+    error_arg = "display"
+  )
+
+  if (!is.null(weight)) {
+    stopifnot(is.character(weight) || is.numeric(weight))
+    weight <- sort(weight)
+  }
+
+  weight_list <- as.character(weight)
+  style_map <- c(normal = "", italic = "i")
+  ital <- sort(style_map[style])
+  
+  values <- character(0)
+  if (length(weight_list) > 0 && length(ital) > 0) {
+    values <- outer(weight_list, ital, paste0) |> as.vector()
+  } else if (length(weight_list) > 0) {
+    values <- weight_list
+  } else if (length(ital) > 0) {
+    values <- ifelse(ital == "", "regular", "italic")
+  }
+  
+  family_values <- ""
+  if (length(values) > 0) {
+    family_values <- paste0(":", paste(values, collapse = ","))
+  }
+  
+  params <- list(
+    family = paste0(family, family_values),
+    display = display
+  )
+  
+  url_base <- "https://fonts.bunny.net/css"
+  url_query <- paste0(names(params), "=", utils::URLencode(unlist(params)), collapse = "&")
+  
+  url <- paste0(url_base, "?", url_query)
+  
+  font_link(family, url)
+}
+
+brand_font_file <- function(family, files, brand_root = getwd()) {
+  if (!(is.list(files) && length(files) > 0)) {
+    abort(c(
+      sprintf("Font family '%s' must have one or more associated files."),
+      "i" = "Use `source: system` for fonts that are provided by the user's system."
+    ))
+  }
+
+  font_collection_files <- lapply(files, function(file) {
+    if (is.null(file$path)) {
+      abort(sprintf("All font `files` for font family '%s' must have a `path`."))
+    }
+
+    font_path <- file.path(brand_root, file$path)
+    font_data_uri <- base64enc::dataURI(
+      file = font_path,
+      mime = mime::guess_type(font_path)
+    )
+    font_type <- switch(
+      sub(".+[.]([a-z0-9]+)$", "\\1", tolower(font_path)),
+      # otc = "collection",
+      # ttc = "collection",
+      # eot = "embedded-opentype",
+      otf = "opentype",
+      ttf = "truetype",
+      # svg = "svg",
+      # svgz = "svg",
+      woff = "woff",
+      woff2 = "woff2",
+      abort(c(
+        sprintf("Invalid font type: %s", font_path),
+        "i" = "Font must be `.ttf`, `.otf`, `.woff` or `.woff2`."
+      ))
+    )
+
+    sass::font_face(
+      family = family,
+      src = sprintf("url(%s) format(%s)", font_data_uri, font_type),
+      weight = brand_remap_font_weight(file$weight),
+      style = file$style,
+      display = "auto"
+    )
+  })
+
+  sass::font_collection(!!!font_collection_files)
+}
+
+brand_remap_font_weight <- function(x) {
+  if (is.null(x)) return()
+  
+  for (i in seq_along(x)) {
+    if (x[[i]] %in% names(brand_font_weight_map)) {
+      x[[i]] <- brand_font_weight_map[x[[i]]]
+    }
+  }
+
+  x
+}
+
+brand_font_weight_map <- c(
+  "thin" = 100,
+  "extra-light" = 200,
+  "ultra-light" = 200,
+  "light" = 300,
+  "normal" = 400,
+  "regular" = 400,
+  "medium" = 500,
+  "semi-bold" = 600,
+  "demi-bold" = 600,
+  "bold" = 700,
+  "extra-bold" = 800,
+  "ultra-bold" = 800,
+  "black" = 900
+)
 
 #' Convert a font size to rem
 #' 
@@ -331,9 +515,10 @@ read_brand_yml <- function(path = NULL) {
 
   # Normalize brand internals !! DOES NOT VALIDATE !!
   brand <- brand_normalize_meta(brand)
-
+  
   class(brand) <- "brand_yml"
-
+  
+  brand$path <- path
   brand
 }
 
