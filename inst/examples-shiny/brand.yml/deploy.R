e0044616c78eeed1b6662de482d20a53e9ed6b44
Garrick Aden-Buie <garrick@adenbuie.com>
2025-01-07 16:24:12 -0500

    fix: add `markdown` to required packages

--- inst/examples-shiny/brand.yml/deploy.R
+++ inst/examples-shiny/brand.yml/deploy.R
@@ -1,5 +1,5 @@
 withr::local_temp_libpaths()
-install.packages(c("shiny", "ggplot2", "yaml", "thematic"))
+install.packages(c("shiny", "ggplot2", "yaml", "thematic", "markdown"))
 
 rsconnect::deployApp(
   rprojroot::find_package_root_file("inst/examples-shiny/brand.yml"),
