5df3e1cad0b3c4b4d5e37f97ba715c40245b4058
Garrick Aden-Buie <garrick@adenbuie.com>
2025-01-07 16:23:44 -0500

    fix: use `brand.yml` for deployed example app name too

--- inst/examples-shiny/brand.yml/deploy.R
+++ inst/examples-shiny/brand.yml/deploy.R
@@ -3,7 +3,7 @@ install.packages(c("shiny", "ggplot2", "yaml", "thematic"))
 
 rsconnect::deployApp(
   rprojroot::find_package_root_file("inst/examples-shiny/brand.yml"),
-  appName = "brand",
+  appName = "brand.yml",
   account = "bslib",
   forceUpdate = TRUE
 )
