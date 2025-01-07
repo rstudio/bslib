49d3abbf993c2cdeeb694836651544a96fb39d89
Garrick Aden-Buie <garrick@adenbuie.com>
2025-01-07 14:06:38 -0500

    fix: path to deploy brand.yml example app

--- inst/examples-shiny/brand.yml/deploy.R
+++ inst/examples-shiny/brand.yml/deploy.R
@@ -2,7 +2,7 @@ withr::local_temp_libpaths()
 install.packages(c("shiny", "ggplot2", "yaml", "thematic"))
 
 rsconnect::deployApp(
-  rprojroot::find_package_root_file("inst/examples-shiny/brand"),
+  rprojroot::find_package_root_file("inst/examples-shiny/brand.yml"),
   appName = "brand",
   account = "bslib",
   forceUpdate = TRUE
