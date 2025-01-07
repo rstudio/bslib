471a49cd8dc0ad01880b67ae9328ee49c7e8b5b8
Garrick Aden-Buie <garrick@adenbuie.com>
2025-01-07 17:18:18 -0500

    fix(example): brand.yml deployed name (#1158)

--- inst/examples-shiny/brand.yml/deploy.R
+++ inst/examples-shiny/brand.yml/deploy.R
@@ -3,7 +3,7 @@ install.packages(c("shiny", "ggplot2", "yaml", "thematic", "markdown"))
 
 rsconnect::deployApp(
   rprojroot::find_package_root_file("inst/examples-shiny/brand.yml"),
-  appName = "brand.yml",
+  appName = "brand-yml",
   account = "bslib",
   forceUpdate = TRUE
 )
