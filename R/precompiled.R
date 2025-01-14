4752721c47f05e8c7031d318fae71cd15eafffdc
Garrick Aden-Buie <garrick@adenbuie.com>
2025-01-14 13:36:59 -0500

    chore: make `.precompiled_css_versions()` a function for runtime checks

--- R/precompiled.R
+++ R/precompiled.R
@@ -1,6 +1,13 @@
-
 # List which precompiled CSS files are available.
-.precompiled_css_versions <- dir(system.file("css-precompiled", package = "bslib"))
+.precompiled_css_versions <- local({
+  versions <- NULL
+  function() {
+    if (is.null(versions)) {
+      versions <<- dir(system.file("css-precompiled", package = "bslib"))
+    }
+    versions
+  }
+})
 .precompiled_css_themes <- new.env(parent = emptyenv())
 
 # We'll need a copy of the `bs_theme()` object representing each precompiled
@@ -8,7 +15,7 @@
 # if the two are identical. Base themes are hashed and cached.
 precompiled_bs_theme_hash <- function(version) {
   theme_hash <- get0(version, .precompiled_css_themes)
-  
+
   if (is.null(theme_hash)) {
     theme_hash <- rlang::hash(bs_theme(version = version, brand = FALSE))
     assign(version, theme_hash, envir = .precompiled_css_themes)
@@ -34,21 +41,26 @@ precompiled_css_path <- function(theme = bs_theme()) {
   built_version <- NULL
 
   if (is.null(version)) {
-    for (v in .precompiled_css_versions) {
+    for (v in .precompiled_css_versions()) {
       if (identical(theme_hash, precompiled_bs_theme_hash(v))) {
         built_version <- v
         break
       }
     }
-  } else if (version %in% .precompiled_css_versions) {
-      if (identical(theme_hash, precompiled_bs_theme_hash(version))) {
-        built_version <- version
-      }
+  } else if (version %in% .precompiled_css_versions()) {
+    if (identical(theme_hash, precompiled_bs_theme_hash(version))) {
+      built_version <- version
+    }
   }
 
   if (is.null(built_version)) {
     return(NULL)
   }
 
-  system_file(package = "bslib", "css-precompiled", built_version, "bootstrap.min.css")
+  system_file(
+    package = "bslib",
+    "css-precompiled",
+    built_version,
+    "bootstrap.min.css"
+  )
 }
