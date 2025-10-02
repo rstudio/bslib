dbd62860d2be20739e61217c306f686d961cc193
Garrick Aden-Buie <garrick@adenbuie.com>
2025-10-02 11:23:18 -0400

    chore: Deprecate `sidebar_toggle()` (#1235)
    * chore: Deprecate `sidebar_toggle()`
    
    * chore: Add NEWS bullet
    
    * `usethis::use_tidy_description()` (GitHub Actions)
    
    * Resave data (GitHub Action)
    
    * Update website deps (GitHub Action)
    
    ---------
    
    Co-authored-by: gadenbuie <gadenbuie@users.noreply.github.com>

--- R/sidebar.R
+++ R/sidebar.R
@@ -459,11 +459,6 @@ toggle_sidebar <- function(id, open = NULL, session = get_current_session()) {
   session$onFlush(callback, once = TRUE)
 }
 
-#' @rdname sidebar
-#' @usage NULL
-#' @export
-sidebar_toggle <- toggle_sidebar
-
 collapse_icon <- function() {
   if (!is_installed("bsicons")) {
     icon <- "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\" class=\"bi bi-chevron-left collapse-icon\" style=\"fill:currentColor;\" aria-hidden=\"true\" role=\"img\" ><path fill-rule=\"evenodd\" d=\"M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z\"></path></svg>"
