f235285c52f6b6c1ef4a304aff284a38ef7e14b7
Garrick Aden-Buie <garrick@adenbuie.com>
2025-10-27 15:18:24 -0400

    chore: rebuild data and component js/css

--- inst/components/dist/components.js
+++ inst/components/dist/components.js
@@ -1,4 +1,4 @@
-/*! bslib 0.9.0.9000 | (c) 2012-2025 RStudio, PBC. | License: MIT + file LICENSE */
+/*! bslib 0.9.0.9001 | (c) 2012-2025 RStudio, PBC. | License: MIT + file LICENSE */
 "use strict";
 (() => {
   var __defProp = Object.defineProperty;
