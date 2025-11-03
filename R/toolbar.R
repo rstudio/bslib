ed1a07e8ce820adc858149862cecc90d996eb326
Liz Nelson <liz.nelson@posit.co>
2025-11-03 11:15:13 -0500

    Updates

--- R/toolbar.R
+++ R/toolbar.R
@@ -1,13 +1,16 @@
 #' Add a toolbar to a UI element
 #'
 #' @description
-#' Display additional information when focusing (or hovering over) a UI element.
+#' A toolbar which can contain buttons, inputs, and other UI elements
+#' in a small form suitable for inclusion in card headers,
+#' footers, and other small places.
 #'
 #' @param ... UI elements for the toolbar.
-#' @param align A character string.
-#' @param size STUFF HERE
+#' @param align Determines if toolbar should be aligned to the right or left.
+#' Must be one of "right" or "left".
+#' @param size The size of the toolbar. Must be one of "sm", "md", or "lg".
 #'
-#' @return Returns a toolbar container.
+#' @return Returns a toolbar.
 #'
 #' @export
 toolbar <- function(
