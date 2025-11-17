3cc95fe0d1c35ac6030027970bc2f3f27b9b9359
Liz Nelson <liz.nelson@posit.co>
2025-11-17 15:11:06 -0500

    Updated tests

--- tests/testthat/test-toolbar.R
+++ tests/testthat/test-toolbar.R
@@ -2,39 +2,26 @@ test_that("toolbar() basic attributes and defaults", {
   tb <- as.tags(toolbar(htmltools::span("Test")))
   expect_match(htmltools::tagGetAttribute(tb, "class"), "bslib-toolbar")
   expect_match(htmltools::tagGetAttribute(tb, "data-align"), "right")
-})
 
-test_that("toolbar() assigns correct attributes", {
   tb <- as.tags(toolbar(align = "left"))
   expect_equal(htmltools::tagGetAttribute(tb, "data-align"), "left")
-})
 
-test_that("toolbar() validation of inputs", {
   expect_error(toolbar("x", align = "center"))
 })
 
-test_that("toolbar() markup snapshots", {
-  show_raw_html <- function(x) {
-    cat(format(x))
-  }
 
+test_that("toolbar() markup snapshots", {
   # Basic toolbar using defaults
-  expect_snapshot(
-    show_raw_html(
-      toolbar("Item 1", "Item 2")
-    )
+  expect_snapshot_html(
+    toolbar("Item 1", "Item 2")
   )
 
   # Toolbars with alignment options
-  expect_snapshot(
-    show_raw_html(
-      toolbar("Item 1", "Item 2", align = "left")
-    )
+  expect_snapshot_html(
+    toolbar("Item 1", "Item 2", align = "left")
   )
 
-  expect_snapshot(
-    show_raw_html(
-      toolbar("Item 1", "Item 2", align = "right")
-    )
+  expect_snapshot_html(
+    toolbar("Item 1", "Item 2", align = "right")
   )
 })
