e147457027967ffbbb106af34c71c1359964f6fb
Liz Nelson <liz.nelson@posit.co>
2025-11-17 19:56:35 -0500

    Updated blocking

--- tests/testthat/test-toolbar.R
+++ tests/testthat/test-toolbar.R
@@ -2,26 +2,19 @@ test_that("toolbar() basic attributes and defaults", {
   tb <- as.tags(toolbar(htmltools::span("Test")))
   expect_match(htmltools::tagGetAttribute(tb, "class"), "bslib-toolbar")
   expect_match(htmltools::tagGetAttribute(tb, "data-align"), "right")
-
-  tb <- as.tags(toolbar(align = "left"))
-  expect_equal(htmltools::tagGetAttribute(tb, "data-align"), "left")
-
-  expect_error(toolbar("x", align = "center"))
-})
-
-
-test_that("toolbar() markup snapshots", {
-  # Basic toolbar using defaults
   expect_snapshot_html(
     toolbar("Item 1", "Item 2")
   )
+})
 
-  # Toolbars with alignment options
+test_that("toolbar() aligns correctly", {
+  tb <- as.tags(toolbar(align = "left"))
+  expect_equal(htmltools::tagGetAttribute(tb, "data-align"), "left")
   expect_snapshot_html(
     toolbar("Item 1", "Item 2", align = "left")
   )
-
   expect_snapshot_html(
     toolbar("Item 1", "Item 2", align = "right")
   )
+  expect_error(toolbar("x", align = "center"))
 })
