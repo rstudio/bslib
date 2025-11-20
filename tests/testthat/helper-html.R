cd2a74620aa9cabcbae1a5c23cedc6494ba64e5e
Liz Nelson <liz.nelson@posit.co>
2025-11-20 12:38:42 -0500

    removing comment

--- tests/testthat/helper-html.R
+++ tests/testthat/helper-html.R
@@ -3,8 +3,6 @@ show_raw_html <- function(x) {
 }
 
 expect_snapshot_html <- function(x, .envir = parent.frame()) {
-  # Use deparse1() with collapse to ensure single-line output
-  # This avoids issues with trailing commas from multi-line deparse
   x_str <- deparse1(substitute(x), collapse = " ")
   code <- parse(text = sprintf("expect_snapshot(show_raw_html(%s))", x_str))
   eval(code, envir = .envir)
