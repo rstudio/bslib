f37b0471faacc1c2328adb08659b979c7c24aede
Garrick Aden-Buie <garrick@adenbuie.com>
2026-08-21 11:27:47 -0400

    test(toolbar): fix label id expectation to use double-dash pattern

--- tests/testthat/test-toolbar.R
+++ tests/testthat/test-toolbar.R
@@ -295,9 +295,9 @@ test_that("toolbar_input_select() has proper label structure", {
   label_elem <- tagQuery(tis)$find("label")$selectedTags()[[1]]
   expect_true(!is.null(label_elem))
 
-  # Label should have id matching pattern "{id}-label"
+  # Label should have id matching pattern "{id}--label"
   label_id <- htmltools::tagGetAttribute(label_elem, "id")
-  expect_equal(label_id, "select-label")
+  expect_equal(label_id, "select--label")
 
   # Label should have for attribute pointing to select
   label_for <- htmltools::tagGetAttribute(label_elem, "for")
